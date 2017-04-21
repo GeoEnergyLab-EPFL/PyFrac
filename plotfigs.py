5ad946ccd3b18c855bcf8cb03fdf4e715e6060c6
Haseeb Zia <haseeb.zia@epfl.ch>
2017-04-21 17:47:55 +0200

    added plotfigs file. (an example for plotting saved data)

---
+++ plotfigs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 15 11:18:48 2016
+
+@author: zia
+"""
+
+from PostProcess import *
+
+
+fig_wdth, fig_radius, fig_pressure = plot_data(".\Data\LamCourse", loglog = False, plot_w_prfl=True, plot_p_prfl=True)
+fig_wdth, fig_radius, fig_pressure = plot_data(".\Data\LamFine", fig_w_cntr=fig_wdth, fig_r=fig_radius, loglog=False )
+fig_wdth, fig_radius, fig_pressure = plot_data(".\Data\TurbLam", fig_w_cntr=fig_wdth, fig_r=fig_radius, loglog=False,
+                                               plot_w_prfl=True, plot_p_prfl=True)
+plt.show()
+
