e801b08902eb2cc33845125a1357aaf8f46b9177
Haseeb Zia <haseeb.zia@epfl.ch>
2018-03-13 16:14:25 +0100

    added a function to make movie from images

--- src/Utility.py
+++ src/Utility.py
@@ -159,3 +159,30 @@ def find_regime(w, Fr, Material_properties, sim_properties, timeStep, Kprime, as
     regime[np.where(regime > 1.)[0]] = 1.
 
     return regime
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+
+def save_images_to_video(image_folder, video_name='movie'):
+
+    import cv2
+    import os
+
+    if ".avi" not in video_name:
+        video_name = video_name + '.avi'
+
+    images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+    frame = cv2.imread(os.path.join(image_folder, images[0]))
+    height, width, layers = frame.shape
+
+    video = cv2.VideoWriter(video_name, -1, 1, (width,height))
+
+    img_no = 0
+    for image in images:
+        print("adding image no " + repr(img_no))
+        video.write(cv2.imread(os.path.join(image_folder, image)))
+        cv2.waitKey(1)
+        img_no += 1
+
+    cv2.destroyAllWindows()
+    video.release()
