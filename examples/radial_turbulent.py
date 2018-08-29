f64ce45fa1cbe89161dc93f5eb5a4574ef86426b
bricelecampion <brice.lecampion@epfl.ch>
2018-08-29 21:04:21 +0200

    MDR app fluid model + HF analytical solution

--- examples/radial_turbulent.py
+++ examples/radial_turbulent.py
@@ -14,20 +14,20 @@ from src.PostProcess import *
 from src.PostProcessFracture import *
 
 # creating mesh
-Mesh = CartesianMesh(3, 3, 45, 45)
+Mesh = CartesianMesh(3, 3, 51, 51)
 
 # solid properties
 nu = 0.                            # Poisson's ratio
 youngs_mod = 2.0e10                 # Young's modulus
 Eprime = youngs_mod / (1 - nu ** 2) # plain strain modulus
-K_Ic = 1.e6                          # fracture toughness
+K_Ic = 2.5e6                          # fracture toughness
 
 Solid = MaterialProperties(Mesh,
                            Eprime,
                            K_Ic)
 
 # injection parameters
-Q0 = 0.211983  # massive injection rate - 80 BPM
+Q0 = 0.079 #0.211983  # massive injection rate - 80 BPM
 Injection = InjectionProperties(Q0, Mesh)
 
 
@@ -37,15 +37,16 @@ Fluid = FluidProperties(viscosity=1.e-3,
 
 tmk = (Eprime**(13/2.)*(12.*Fluid.viscosity)**(5./2)*(Q0**(3./2.)))/(((32./np.pi)**0.5*K_Ic)**9.)  # transition M to K vertex
 
+
 # simulation properties
 simulProp = SimulationParameters()
-simulProp.FinalTime = 500.             # the time at which the simulation stops
+simulProp.FinalTime = 1000.             # the time at which the simulation stops
 simulProp.set_tipAsymptote('U')         # the tip asymptote is evaluated with the toughness dominated assumption
 simulProp.frontAdvancing = 'implicit'   # to set explicit front tracking
 simulProp.outputTimePeriod = 1e-4       # to save after every steps
-simulProp.set_outFileAddress("./Data/MDR_r1") # the disk address where the files are saved
+simulProp.set_outFileAddress("./Data/MDR_test") # the disk address where the files are saved
 simulProp.plotFigure = True
-simulProp.plotAnalytical = True
+simulProp.plotAnalytical = False
 simulProp.analyticalSol = 'MDR'
 simulProp.saveReynNumb = True
 simulProp.verbosity = 1
@@ -94,16 +95,18 @@ controller.run()
 
 # plot results
 Fr_list = load_fractures(simulProp.get_outFileAddress(), multpl_sim=True,
-               time_srs=np.linspace(0.0028,0.5,30))
+               time_srs=(np.linspace(0.0028,1000,40)))
 
 fig1,fig2=plot_radius(simulProp.get_outFileAddress(), multpl_sim=True,
                         r_type='mean',
-                        analytical_sol='M',add_labels=False,anltcl_lnStyle='r')
+                        analytical_sol='K',add_labels=False,anltcl_lnStyle='r')
 
 plot_radius(simulProp.get_outFileAddress(), multpl_sim=True,
                         r_type='mean',fig_r=fig1,fig_err=fig2,
                         analytical_sol='MDR',add_labels=False)
 plt.show()
+
+plot_fracture_volume(simulProp.get_outFileAddress())
 
 t, R, p, w, v, actvElts =MDR_M_vertex_solution(Eprime,Q0,density=Fluid.density,visc=Fluid.viscosity,Mesh=Mesh,t=Fr_list[5].time)
 
@@ -111,11 +114,18 @@ for ff in Fr_list:
     fig=plot_Reynolds_number(ff,1810)
     #plot_pressure_contour(ff)
     # fig=plot_width_contour(ff,bck_colMap='jet')
-    # plot_footprint(simulProp.get_outFileAddress(),  # the address where the results are stored
-    #                plot_at_times=ff.time,  # the time series at which the solution is plotted
-    #                plt_mesh=False,fig=fig,plt_color='r') #
+    plot_footprint(simulProp.get_outFileAddress(),  # the address where the results are stored
+                   plot_at_times=ff.time,  # the time series at which the solution is plotted
+                   plt_mesh=False,fig=fig,plt_color='r') #
     plt.show()
 
+
+plot_as_matrix(Fr_list[-1].ReynoldsNumber[0],Mesh)
+
+
+plot_as_matrix(Fr_list[1].ReynoldsNumber[0],Mesh)
+
+PrintDomain(Fr_list[10].ReynoldsNumber[0], Mesh)
 
 plot_footprint(simulProp.get_outFileAddress(),         # the address where the results are stored
                         plot_at_times=np.linspace(0.0028,0.031,7),# the time series at which the solution is plotted
@@ -130,24 +140,32 @@ plt.show()
 
 
 plot_at_injection_point(simulProp.get_outFileAddress(), multpl_sim=True,
-                        plt_pressure=False,
+                        plt_pressure=True,
                         analytical_sol='MDR')
 plt.show()
 
 
 f1,f2,f3,f4 =plot_profile(simulProp.get_outFileAddress(),multpl_sim=True,
-                        plot_at_times=np.linspace(0.0028,0.5,5),
-                        analytical_sol='MDR') #,plt_pressure=True
+                        plot_at_times=np.linspace(0.0032,1000,6),
+                        analytical_sol='K') #,plt_pressure=True
 
 plt.show()
 
 
+# plot width at early time - compare with MDR
 plot_profile(simulProp.get_outFileAddress(),
-                        plot_at_times=np.linspace(0.0028,0.031,5), plt_symbol=".-b")
+                        plot_at_times=np.array([ 0.0032,  0.1, 0.32,1,5 ]),#np.linspace(0.0032,0.2,5),
+             plt_symbol=".",
+             analytical_sol='MDR',anltcl_lnStyle='k')
 plt.show()
+# how to get the exact corresponding time ?
 
 
-plot_profile(simulProp.get_outFileAddress(), plt_symbol=".-b")
+# plot width at late time - compare with K
+plot_profile(simulProp.get_outFileAddress(),
+                        plot_at_times=np.array([ 100,200,600,1000 ]),#np.linspace(0.0032,0.2,5),
+             plt_symbol=".",
+             analytical_sol='K',anltcl_lnStyle='r')
 plt.show()
 
 
