323def296176bf1b0172f0f3fc5a820fa38fab50
HaseebZia <haseeb.zia@epfl.ch>
2019-03-18 18:52:11 +0100

    added a buoyancy driven fracture example with line source

---
+++ examples/buoyancy_line_source.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+This file is part of PyFrac.
+
+Created by Haseeb Zia on Fri June 16 17:49:21 2017.
+Copyright (c) "ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, Geo-Energy Laboratory", 2016-2019. All rights
+reserved. See the LICENSE.TXT file for more details.
+"""
+
+# imports
+from src.Fracture import *
+from src.Controller import *
+
+
+# creating mesh
+Mesh = CartesianMesh(100, 150, 35, 61)
+
+# solid properties
+nu = 0.4                            # Poisson's ratio
+youngs_mod = 3.3e10                 # Young's modulus
+Eprime = youngs_mod / (1 - nu ** 2) # plain strain modulus
+K_Ic = 0.5                          # fracture toughness
+
+def sigmaO_func(x, y):
+    """ This function provides the confining stress over the domain"""
+
+    # only dependant on the depth
+    density = 2000
+    return -(y - 400) * density * 9.8
+
+
+# material properties
+Solid = MaterialProperties(Mesh,
+                           Eprime,
+                           K_Ic,
+                           confining_stress_func=sigmaO_func)
+
+def source_location(x, y):
+    """ This function is used to evaluate if a point is included in source, i.e. the fluid is injected at the given
+        point.
+    """
+    # the condition
+    return abs(x) < 75. and (y > -80. and y < -74)
+
+# injection parameters
+Q0 = 0.001  # injection rate
+Injection = InjectionProperties(Q0, Mesh, source_loc_func=source_location)
+
+# fluid properties
+Fluid = FluidProperties(viscosity=1.1e-3, density=1000)
+
+# simulation properties
+simulProp = SimulationProperties()
+simulProp.finalTime = 4000               # the time at which the simulation stops
+simulProp.set_tipAsymptote('M')         # the tip asymptote is evaluated with the viscosity dominated assumption
+simulProp.set_outputFolder("./Data/M_radial_explicit") # the disk address where the files are saved
+simulProp.height = 32
+simulProp.gravity = True
+simulProp.bckColor = 'confining stress'
+simulProp.toleranceEHL = 1e-4
+
+# initialization parameters
+initRad = 85
+init_param = ("PKN", "length", initRad, 32)
+# init_param = ("M", "length", initRad)
+
+# creating fracture object
+Fr = Fracture(Mesh,
+              init_param,
+              Solid,
+              Fluid,
+              Injection,
+              simulProp)
+
+
+# create a Controller
+controller = Controller(Fr,
+                        Solid,
+                        Fluid,
+                        Injection,
+                        simulProp)
+
+# run the simulation
+controller.run()
+
+
+####################
+# plotting results #
+####################
+
+# loading simulation results
+time_srs = np.linspace(1, 4000, 5)
+Fr_list, properties = load_fractures(address="./Data/M_radial_explicit",
+                                     time_srs=time_srs)
+time_srs = get_fracture_variable(Fr_list,
+                                 variable='time')
+
+# plot footprint
+Fig_FP = plot_fracture_list(Fr_list,
+                            variable='mesh',
+                            projection='2D')
+Fig_FP = plot_fracture_list(Fr_list,
+                            variable='footprint',
+                            projection='2D',
+                            fig=Fig_FP)
+
+# plot slice
+plot_prop = PlotProperties(line_style='.-')
+Fig_WS = plot_fracture_list_slice(Fr_list,
+                                  variable='w',
+                                  projection='2D',
+                                  plot_prop=plot_prop,
+                                  plot_cell_center=True,
+                                  orientation='vertical')
+
+#plotting in 3D
+Fig_Fr = plot_fracture_list(Fr_list,
+                            variable='mesh',
+                            projection='3D')
+Fig_Fr = plot_fracture_list(Fr_list,
+                            variable='width',
+                            projection='3D',
+                            fig=Fig_Fr)
+Fig_Fr = plot_fracture_list(Fr_list,
+                            variable='footprint',
+                            projection='3D',
+                            fig=Fig_Fr)
+
+plt.show(block=True)
+
