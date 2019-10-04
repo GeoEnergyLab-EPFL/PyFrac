539feee1dc1fd59c91f6d25ae45742c7ca728da0
HaseebZia <haseeb.zia@epfl.ch>
2019-10-04 16:26:01 +0200

    with multithreaded matrix vector product

--- src/explicit_RKL.py
+++ src/explicit_RKL.py
@@ -279,14 +279,14 @@ def RKL_substep_2(j, s, W_jm1, W_jm2, W_0, crack, n_channel, tip_delw_step, para
 
     return W_j, sum_mut, sum_gamma
 
-
+@profile
 def solve_with_RKL2_neg(Eprime, *args):
 
     (to_solve, to_impose, wLastTS, pfLastTS, imposed_val, EltCrack, Mesh, dt, Q, C, muPrime, rho, InCrack, LeakOff,
      sigma0, turb, dgrain, gravity, active, wc_to_impose, wc, cf, neiInCrack) = args
 
     viscosity = muPrime / 12
-    dt_CFL = 3 * np.min(viscosity) * min(Mesh.hx, Mesh.hy) ** 3 / \
+    dt_CFL = 5 * np.min(viscosity) * min(Mesh.hx, Mesh.hy) ** 3 / \
              (Eprime * np.max(wLastTS) ** 3)
     s = ceil(-0.5 + (8 + 16 * dt / dt_CFL) ** 0.5 / 2)
     print("s = " + repr(s))
@@ -307,24 +307,24 @@ def solve_with_RKL2_neg(Eprime, *args):
     # tip_indxs = n_ch + n_act + np.arange(n_tip)
 
 
-    cond_0 = finiteDiff_operator_laminar(wLastTS,
+    cond_0_lil = finiteDiff_operator_laminar(wLastTS,
                                          EltCrack,
                                          muPrime,
                                          Mesh,
                                          InCrack,
-                                         neiInCrack)
-
+                                         neiInCrack,
+                                         sparse_flag=True)
+    cond_0 = cond_0_lil.tocsr()
     mu_t_1 = 4 / (3 * (s * s + s - 2))
     # pf_0 = Fr_lstTmStp.pFluid[EltCrack]
     pf_0 = np.empty(len(EltCrack))
     pf_0[ch_indxs] = np.dot(C[np.ix_(to_solve, EltCrack)], wLastTS[EltCrack]) + \
                        sigma0[to_solve]
-    pf_0[n_ch:] = np.linalg.solve(dt * mu_t_1 * cond_0[n_ch:, n_ch:-1],
-                                       act_tip_val - np.dot(dt * mu_t_1 * cond_0[n_ch:, :n_ch],
-                                                              pf_0[:n_ch]))
+    pf_0[n_ch:] = np.linalg.solve(dt * mu_t_1 * (cond_0[n_ch:, n_ch:-1]).toarray(),
+                                       act_tip_val - dt * mu_t_1 * cond_0[n_ch:, :][:, :n_ch].dot(pf_0[:n_ch]))
     # pf_0 = np.dot(C[np.ix_(EltCrack, EltCrack)], wLastTS[EltCrack])
 
-    M_0 = np.dot(cond_0[:, :-1], pf_0) + Q[EltCrack] / Mesh.EltArea
+    M_0 = cond_0[:, :-1].dot(pf_0) + Q[EltCrack] / Mesh.EltArea
 
     W_0 = wLastTS[EltCrack]
 
@@ -334,9 +334,10 @@ def solve_with_RKL2_neg(Eprime, *args):
     W_jm2 = np.copy(W_0)
     tau_M0 = dt * M_0
     param_pack = (muPrime, Mesh, InCrack, neiInCrack)
-
+    C_red = np.asfortranarray(C[np.ix_(to_solve, EltCrack)])
     for j in range(2, s + 1):
-        W_j = RKL_substep_neg(j, s, W_jm1, W_jm2, W_0, EltCrack, n_ch, tip_delw_step, param_pack, C, dt,
+        print('step = ' + repr(j))
+        W_j = RKL_substep_neg(j, s, W_jm1, W_jm2, W_0, EltCrack, n_ch, tip_delw_step, param_pack, C_red, dt,
                           tau_M0, Q[EltCrack], to_solve, sigma0, act_tip_val)
         W_jm2 = W_jm1
         W_jm1 = W_j
@@ -351,8 +352,8 @@ def solve_with_RKL2_neg(Eprime, *args):
 
     return sol, None
 
-
-def RKL_substep_neg(j, s, W_jm1, W_jm2, W_0, crack, n_channel, tip_delw_step, param_pack, C, tau, tau_M0, Qin, EltChannel,
+@profile
+def RKL_substep_neg(j, s, W_jm1, W_jm2, W_0, crack, n_channel, tip_delw_step, param_pack, C_, tau, tau_M0, Qin, EltChannel,
                 sigmaO, imposed_value):
     # pf = np.dot(C[np.ix_(crack, crack)], W_jm1)
     muPrime, Mesh, InCrack, neiInCrack = param_pack
@@ -360,24 +361,27 @@ def RKL_substep_neg(j, s, W_jm1, W_jm2, W_0, crack, n_channel, tip_delw_step, pa
     cp_W_jm1 = np.copy(W_jm1)
     cp_W_jm1[W_jm1 < 1e-6] = 1e-6
     w_jm1[crack] = cp_W_jm1
-    cond = finiteDiff_operator_laminar(w_jm1, crack, muPrime, Mesh, InCrack, neiInCrack)
+    cond_lil = finiteDiff_operator_laminar(w_jm1, crack, muPrime, Mesh, InCrack, neiInCrack, sparse_flag=True)
+    cond = cond_lil.tocsr()
     mu_t = 4 * (2 * j - 1) * b[j] / (j * (s * s + s - 2) * b[j - 1])
     gamma_t = -a[j - 1] * mu_t
 
     # pf = np.dot(C[np.ix_(crack, crack)], W_jm1)
 
     pf = np.empty(len(crack))
-    pf[:n_channel] = np.dot(C[np.ix_(EltChannel, crack)], W_jm1) + sigmaO[EltChannel]
-
+    # pf[:n_channel] = pardot(C_, W_jm1, 4, 2) + sigmaO[EltChannel]
+    # pf[:n_channel] = np.dot(C_, W_jm1) + sigmaO[EltChannel]
+    pf[:n_channel] = pardot_matrix_vector(C_, W_jm1, 4) + sigmaO[EltChannel]
     imposed_value[-len(tip_delw_step):] = j * tip_delw_step
     S = imposed_value - mu[j] * W_jm1[n_channel:] - nu[j] * W_jm2[n_channel:] + (mu[j] + nu[j]) * W_0[
                             n_channel:] - gamma_t * tau_M0[n_channel:] - mu_t * tau * np.dot(
-                            cond[n_channel:, :n_channel], pf[:n_channel])
+                            (cond[n_channel:, :][:, :n_channel]).toarray(), pf[:n_channel])
 
-    A = mu_t * tau * cond[n_channel:, n_channel:-1]
+    A = mu_t * tau * (cond[n_channel:, :][:, n_channel:-1]).toarray()
     pf[n_channel:] = np.linalg.solve(A, S)
 
-    M_jm1 = np.dot(cond[:, :-1], pf) + Qin / Mesh.EltArea
+    M_jm1 = cond[:, :-1].dot(pf) + Qin / Mesh.EltArea
+
 
     # W_j[:n_channel] = mu[j] * W_jm1[:n_channel] + nu[j] * W_jm2[:n_channel] + (1 - mu[j] - nu[j]) * W_0[:n_channel] + mu_t * tau * M_jm1 + gamma_t * tau_M0
     W_j = mu[j] * W_jm1 + nu[j] * W_jm2 + (1 - mu[j] - nu[j]) * W_0 + mu_t * tau * M_jm1 + gamma_t * tau_M0
@@ -385,3 +389,85 @@ def RKL_substep_neg(j, s, W_jm1, W_jm2, W_0, crack, n_channel, tip_delw_step, pa
     # W_j[n_channel:] = s * tip_delw_step
 
     return W_j
+
+import threading
+
+def blockshaped(arr, nrows, ncols):
+    """
+    Return an array of shape (nrows, ncols, n, m) where
+    n * nrows, m * ncols = arr.shape.
+    This should be a view of the original array.
+    """
+    h, w = arr.shape
+    n, m = h // nrows, w // ncols
+    return arr.reshape(nrows, n, ncols, m).swapaxes(1, 2)
+
+
+def do_dot(a, b, out):
+    # np.dot(a, b, out)  # does not work. maybe because out is not C-contiguous?
+    out[:] = np.dot(a, b)  # less efficient because the output is stored in a temporary array?
+
+
+def pardot(a, b, nblocks, mblocks, dot_func=do_dot):
+    """
+    Return the matrix product a * b.
+    The product is split into nblocks * mblocks partitions that are performed
+    in parallel threads.
+    """
+    n_jobs = nblocks * mblocks
+    print('running {} jobs in parallel'.format(n_jobs))
+
+    if len(b.shape) == 1:
+        b = b.reshape((b.shape[0], 1))
+
+    out = np.empty((a.shape[0], b.shape[1]), dtype=a.dtype)
+
+    out_blocks = blockshaped(out, nblocks, mblocks)
+    a_blocks = blockshaped(a, nblocks, 1)
+    b_blocks = blockshaped(b, 1, mblocks)
+
+    threads = []
+    for i in range(nblocks):
+        for j in range(mblocks):
+            th = threading.Thread(target=dot_func,
+                                  args=(a_blocks[i, 0, :, :],
+                                        b_blocks[0, j, :, :],
+                                        out_blocks[i, j, :, :]))
+            th.start()
+            threads.append(th)
+
+    for th in threads:
+        th.join()
+
+    return out
+@profile
+def pardot_matrix_vector(a, b, nblocks, dot_func=do_dot):
+    """
+    Return the matrix product a * b.
+    The product is split into nblocks * mblocks partitions that are performed
+    in parallel threads.
+    """
+    n_jobs = nblocks
+    # print('running {} jobs in parallel'.format(n_jobs))
+
+    out = np.empty((a.shape[0]), dtype=a.dtype)
+
+    n_in_block = a.shape[0] // nblocks
+    out_blocks = out[:n_in_block * nblocks].reshape((nblocks, n_in_block))
+    a_blocks = a[:n_in_block * nblocks, :].reshape((nblocks, n_in_block, a.shape[1]))
+
+    threads = []
+    for i in range(nblocks):
+        th = threading.Thread(target=dot_func,
+                              args=(a_blocks[i, :, :],
+                                    b,
+                                    out_blocks[i, :]))
+        th.start()
+        threads.append(th)
+
+    for th in threads:
+        th.join()
+
+    out[n_in_block * nblocks:] = np.dot(a[n_in_block * nblocks:, :], b)
+
+    return out
