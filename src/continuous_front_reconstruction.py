Corrected a bug in the computation of the pressure gradient. Now is possible to plot the fluid flux and the fluid velocity with vectors in the case of laminar flow.

src/continuous_front_reconstruction.py
@@ -538,8 +538,6 @@ def find_fictitius_cells(anularegion, NeiElements, sgndDist_k):
                       np.greater(LS_a_times_LS_c, 0.).astype(int) +
                       np.greater(LS_b_times_LS_i, 0.).astype(int) ) == 4)[0]]
 
-        if i_indexes_of_TYPE_2_cells.size>0:
-            print("here")
         """
         Whe define the fictitius cell types:
         
@@ -1892,6 +1890,7 @@ def reconstruct_front_continuous(sgndDist_k, anularegion, Ribbon, eltsChannel, m
                 del dict_FC_names[next(iter(dict_FC_names))]
                 NofCells_explored += 1
 
+                # todo: in case of cells of type 2 this have to be reviewed: do not delete cell of type 2
                 next_cell_name = get_next_cell_name_from_first(first_cell_name,i_1_2_3_4_FC_type[str(first_cell_name)],mesh,sgndDist_k)
 
                 while next_cell_name != first_cell_name :
@@ -1913,7 +1912,7 @@ def reconstruct_front_continuous(sgndDist_k, anularegion, Ribbon, eltsChannel, m
                         cell_type = i_1_2_3_4_FC_type[str(next_cell_name)]
 
                     [Cells_type_1_list, Cells_type_2_list, Cells_type_3_list, Cells_type_4_list] = append_to_typelists(len(Fracturelist) - 1, cell_type, Cells_type_1_list, Cells_type_2_list, Cells_type_3_list, Cells_type_4_list)
-                    if str(next_cell_name) in dict_FC_names.keys():
+                    if str(next_cell_name) in dict_FC_names.keys() and cell_type != 2 :
                         del dict_FC_names[str(next_cell_name)]
                     previous_cell_name = Fracturelist[-2] #second last cell in the list and the one where we are coming
                     next_cell_name = get_next_cell_name(next_cell_name,previous_cell_name,cell_type,Args)

src/elastohydrodynamic_solver.py
@@ -1418,10 +1418,23 @@ def pressure_gradient(w, C, sigma0, Mesh, EltCrack, InCrack):
 
     return dpdxLft, dpdxRgt, dpdyBtm, dpdyTop
 
+#-----------------------------------------------------------------------------------------------------------------------
+
+def pressure_gradient_form_pressure(w, pf, sigma0, Mesh, EltCrack, InCrack):
+    """
+    This function gives the pressure gradient at the cell edges evaluated with the pressure
+    """
+
+    dpdxLft = (pf[EltCrack] - pf[Mesh.NeiElements[EltCrack, 0]]) * InCrack[Mesh.NeiElements[EltCrack, 0]]
+    dpdxRgt = (pf[Mesh.NeiElements[EltCrack, 1]] - pf[EltCrack]) * InCrack[Mesh.NeiElements[EltCrack, 1]]
+    dpdyBtm = (pf[EltCrack] - pf[Mesh.NeiElements[EltCrack, 2]]) * InCrack[Mesh.NeiElements[EltCrack, 2]]
+    dpdyTop = (pf[Mesh.NeiElements[EltCrack, 3]] - pf[EltCrack]) * InCrack[Mesh.NeiElements[EltCrack, 3]]
+
+    return dpdxLft, dpdxRgt, dpdyBtm, dpdyTop
 
 #-----------------------------------------------------------------------------------------------------------------------
 
-def calculate_fluid_flow_characteristics_laminar(w, C, sigma0, Mesh, EltCrack, InCrack, muPrime, density):
+def calculate_fluid_flow_characteristics_laminar(w, pf, sigma0, Mesh, EltCrack, InCrack, muPrime, density):
     """
     This function calculate fluid flux and velocity at the cell edges evaluated with the pressure calculated from the
     elasticity relation for the given fracture width and the poisoille's Law.
@@ -1431,7 +1444,7 @@ def calculate_fluid_flow_characteristics_laminar(w, C, sigma0, Mesh, EltCrack, I
                                              0     1      2      3
     """
     dp = np.zeros((8, Mesh.NumberOfElts), dtype=np.float64)
-    (dpdxLft, dpdxRgt, dpdyBtm, dpdyTop) = pressure_gradient(w, C, sigma0, Mesh, EltCrack, InCrack)
+    (dpdxLft, dpdxRgt, dpdyBtm, dpdyTop) = pressure_gradient_form_pressure(w, pf, sigma0, Mesh, EltCrack, InCrack)
     # dp = [dpdxLft , dpdxRgt, dpdyBtm, dpdyTop, dpdyLft, dpdyRgt, dpdxBtm, dpdxTop]
     dp[0, EltCrack] = dpdxLft
     dp[1, EltCrack] = dpdxRgt

src/postprocess_fracture.py
@@ -221,8 +221,38 @@ def get_fracture_variable(fracture_list, variable, edge=4, return_time=False):
             if edge < 4:
                 variable_list.append(i.fluidVelocity[edge])
                 time_srs.append(i.time)
-            elif i.fluidFlux is not None:
+            elif i.fluidVelocity is not None:
                 variable_list.append(np.mean(i.fluidVelocity, axis=0))
+                time_srs.append(i.time)
+            else:
+                variable_list.append(np.full((i.mesh.NumberOfElts, ), np.nan))
+
+    elif variable == 'fluid flux as vector field' or variable == 'ffvf':
+        if fracture_list[-1].fluidFlux_components is None:
+            raise SystemExit(err_var_not_saved)
+        for i in fracture_list:
+            if edge < 0 or edge > 4:
+                raise ValueError('Edge can be an integer between and including 0 and 4.')
+            if edge < 4:
+                variable_list.append(i.fluidFlux_components[edge])
+                time_srs.append(i.time)
+            elif i.fluidFlux_components is not None:
+                variable_list.append(i.fluidFlux_components)
+                time_srs.append(i.time)
+            else:
+                variable_list.append(np.full((i.mesh.NumberOfElts,), np.nan))
+
+    elif variable == 'fluid velocity as vector field' or variable == 'fvvf':
+        if fracture_list[-1].fluidVelocity_components is None:
+            raise SystemExit(err_var_not_saved)
+        for i in fracture_list:
+            if edge < 0 or edge > 4:
+                raise ValueError('Edge can be an integer between and including 0 and 4.')
+            if edge < 4:
+                variable_list.append(i.fluidVelocity_components[edge])
+                time_srs.append(i.time)
+            elif i.fluidFlux_components is not None:
+                variable_list.append(i.fluidVelocity_components)
                 time_srs.append(i.time)
             else:
                 variable_list.append(np.full((i.mesh.NumberOfElts, ), np.nan))

src/visualization.py
@@ -154,23 +154,23 @@ def plot_fracture_list(fracture_list, variable='footprint', projection=None, ele
         for i in range(len(var_val_copy)):
             var_val_copy[i] /= labels.unitConversion
 
-
-        var_value_tmp = np.copy(var_val_copy)
-        if plot_non_zero:
-            var_value_tmp = var_value_tmp[var_value_tmp != 0]
-        vmin, vmax = np.inf, -np.inf
-        for i in var_value_tmp:
+        if projection != '2D_vectorfield':
+            var_value_tmp = np.copy(var_val_copy)
             if plot_non_zero:
-                i = i[i != 0]
-            i = np.delete(i, np.where(np.isinf(i))[0])
-            i = np.delete(i, np.where(np.isnan(i))[0])
-            if not (not isinstance(i, float) and len(i) == 0):
-                if variable in ('p', 'pressure'):
-                    non_zero = np.where(abs(i) > 0)[0]
-                    i_min, i_max = -0.2 * np.median(i[non_zero]), 1.5 * np.median(i[non_zero])
-                else:
-                    i_min, i_max = np.min(i), np.max(i)
-                vmin, vmax = min(vmin, i_min), max(vmax, i_max)
+                var_value_tmp = var_value_tmp[var_value_tmp != 0]
+            vmin, vmax = np.inf, -np.inf
+            for i in var_value_tmp:
+                if plot_non_zero:
+                    i = i[i != 0]
+                i = np.delete(i, np.where(np.isinf(i))[0])
+                i = np.delete(i, np.where(np.isnan(i))[0])
+                if not (not isinstance(i, float) and len(i) == 0):
+                    if variable in ('p', 'pressure'):
+                        non_zero = np.where(abs(i) > 0)[0]
+                        i_min, i_max = -0.2 * np.median(i[non_zero]), 1.5 * np.median(i[non_zero])
+                    else:
+                        i_min, i_max = np.min(i), np.max(i)
+                    vmin, vmax = min(vmin, i_min), max(vmax, i_max)
 
 
     if variable in unidimensional_variables:
@@ -181,10 +181,10 @@ def plot_fracture_list(fracture_list, variable='footprint', projection=None, ele
                                     label=labels.legend)
 
     elif variable not in ['mesh', 'footprint']:
-
-        if plot_non_zero:
-            for indx, value in enumerate(var_val_copy):
-                remove_zeros(value, fracture_list[indx].mesh)#i[np.where(abs(i) < 1e-16)[0]] = np.nan
+        if projection != '2D_vectorfield':
+            if plot_non_zero:
+                for indx, value in enumerate(var_val_copy):
+                    remove_zeros(value, fracture_list[indx].mesh)#i[np.where(abs(i) < 1e-16)[0]] = np.nan
 
         if variable == 'surface':
             plot_prop.colorMap = 'cool'
@@ -230,6 +230,17 @@ def plot_fracture_list(fracture_list, variable='footprint', projection=None, ele
                                                         elements=elements,
                                                         vmin=vmin,
                                                         vmax=vmax)
+        elif projection == '2D_vectorfield':
+            # fracture_list[i].EltCrack => ribbon+tip+other in crack
+            # fracture_list[i].EltChannel => ribbon+other in crack
+            elements_where_to_plot = fracture_list[i].EltCrack
+            #elements_where_to_plot = fracture_list[i].EltChannel
+            #elements_where_to_plot = np.setdiff1d(fracture_list[i].EltChannel,fracture_list[i].EltRibbon)
+            #elements_where_to_plot = np.setdiff1d(elements_where_to_plot, np.unique(np.ndarray.flatten(fracture_list[i].mesh.NeiElements[fracture_list[i].EltRibbon])))
+            fig = plot_fracture_variable_as_vector(var_val_copy[i],
+                                                      fracture_list[i].mesh,
+                                                      elements_where_to_plot,
+                                                      fig=fig)
 
     ax = fig.get_axes()[0]
     ax.set_xlabel(labels.xLabel)
@@ -492,6 +503,47 @@ def plot_fracture_list_at_point(fracture_list, variable='width', point=None, plo
 
 #-----------------------------------------------------------------------------------------------------------------------
 
+def plot_fracture_variable_as_vector(var_value, mesh, Elements_to_plot, fig=None):
+
+    #todo: complete the description
+    """
+
+
+    """
+
+    if fig is None:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+    else:
+        ax = fig.get_axes()[0]
+
+    U = np.vstack((var_value[0,Elements_to_plot], var_value[2,Elements_to_plot]))
+    U = np.vstack((U, var_value[4,Elements_to_plot]))
+    U = np.vstack((U, var_value[6,Elements_to_plot]))
+    U = np.ndarray.flatten(U)
+
+    V = np.vstack((var_value[1,Elements_to_plot], var_value[3,Elements_to_plot]))
+    V = np.vstack((V, var_value[5,Elements_to_plot]))
+    V = np.vstack((V, var_value[7,Elements_to_plot]))
+    V = np.ndarray.flatten(V)
+
+    X = np.vstack((mesh.CenterCoor[Elements_to_plot,0]-mesh.hx*0.5, mesh.CenterCoor[Elements_to_plot,0]+mesh.hx*0.5))
+    X = np.vstack((X, mesh.CenterCoor[Elements_to_plot,0]))
+    X = np.vstack((X, mesh.CenterCoor[Elements_to_plot,0]))
+    X = np.ndarray.flatten(X)
+
+    Y = np.vstack((mesh.CenterCoor[Elements_to_plot,1], mesh.CenterCoor[Elements_to_plot,1]))
+    Y = np.vstack((Y, mesh.CenterCoor[Elements_to_plot,1]-mesh.hy*0.5))
+    Y = np.vstack((Y, mesh.CenterCoor[Elements_to_plot,1]+mesh.hy*0.5))
+    Y = np.ndarray.flatten(Y)
+
+    M = np.hypot(U, V)
+
+    ax.quiver(X,Y,U,V,M,pivot='mid')
+
+    return fig
+
+#-----------------------------------------------------------------------------------------------------------------------
 
 def plot_variable_vs_time(time_list, value_list, fig=None, plot_prop=None, label=None):
     """