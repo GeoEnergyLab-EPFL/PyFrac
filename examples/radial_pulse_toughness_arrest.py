51a1900977bf4611584c88c5c0b7fa1da52605fb
Andreas Möri <andreas.mori@epfl.ch>
2020-10-01 18:14:10 +0200

    Added examples for the pulse.

--- examples/radial_pulse_toughness_arrest.py
+++ examples/radial_pulse_toughness_arrest.py
@@ -23,7 +23,7 @@ Mesh = CartesianMesh(2, 2, 41, 41)
 nu = 0.4                            # Poisson's ratio
 youngs_mod = 3.3e10                 # Young's modulus
 Eprime = youngs_mod / (1 - nu**2)   # plain strain modulus
-K1c = 0                             # Zero toughness case
+K1c = 1e5                           # Fracture toughness
 
 
 # material properties
@@ -32,7 +32,7 @@ Solid = MaterialProperties(Mesh,
                            K1c)
 
 # injection parameters
-Q0 = np.asarray([[0.0, 50],
+Q0 = np.asarray([[0.0, 5],
                  [0.01, 0]])  # injection rate
 
 Injection = InjectionProperties(Q0, Mesh)
@@ -68,7 +68,7 @@ controller = Controller(Fr,
                         simulProp)
 
 # run the simulation
-#controller.run()
+controller.run()
 
 
 ####################
@@ -120,82 +120,60 @@ Fig_R = plot_analytical_solution(regime='Mp',
                                  plot_prop=plt_prop,
                                  fig=Fig_R)
 
-# plot slice of width
-time_slice = np.asarray([0.5, 5, 45, 5e4, 5e5, 5e6])
-Fr_slice, properties = load_fractures(address="./Data/Mpulse",
-                                      time_srs=time_slice)       # load specific fractures
-time_slice = get_fracture_variable(Fr_slice,
-                                   variable='time')
+# plot the toughness arrest radius
+plt_prop = PlotProperties(line_color_anal='r')
+label = LabelProperties('d_mean')
+label.legend = 'Toughness arrest'
+Fig_R = plot_analytical_solution(regime='Ka',
+                                 variable='d_mean',
+                                 labels=label,
+                                 mat_prop=properties[0],
+                                 inj_prop=properties[2],
+                                 fluid_prop=properties[1],
+                                 time_srs=time_srs,
+                                 plot_prop=plt_prop,
+                                 fig=Fig_R)
 
-ext_pnts = np.empty((2, 2), dtype=np.float64)
-Fig_WS_M = plot_fracture_list_slice(Fr_slice[:3],
-                                  variable='w',
-                                  projection='2D',
-                                  plot_cell_center=True,
-                                  extreme_points=ext_pnts)
-# plot slice of width analytical
-Fig_WS_M = plot_analytical_solution_slice('M',
-                                        'w',
-                                        Solid,
-                                        Injection,
-                                        time_srs=time_slice[:3],
-                                        fluid_prop=Fluid,
-                                        fig=Fig_WS_M,
-                                        point1=ext_pnts[0],
-                                        point2=ext_pnts[1])
-
-
-ext_pnts = np.empty((2, 2), dtype=np.float64)
-Fig_WS_Mp = plot_fracture_list_slice(Fr_slice[3:],
-                                  variable='w',
-                                  projection='2D',
-                                  plot_cell_center=True,
-                                  extreme_points=ext_pnts)
-# plot slice of width analytical
-Fig_WS_Mp = plot_analytical_solution_slice('Mp',
-                                        'w',
-                                        Solid,
-                                        Injection,
-                                        time_srs=time_slice[3:],
-                                        fluid_prop=Fluid,
-                                        fig=Fig_WS_Mp,
-                                        point1=ext_pnts[0],
-                                        point2=ext_pnts[1])
-
-# plot slice of pressure
-ext_pnts = np.empty((2, 2), dtype=np.float64)
-Fig_PS_M = plot_fracture_list_slice(Fr_slice[:3],
-                                  variable='pn',
-                                  projection='2D',
-                                  plot_cell_center=True,
-                                  extreme_points=ext_pnts)
-# plot slice of width analytical
-Fig_PS_M = plot_analytical_solution_slice('M',
-                                        'pn',
-                                        Solid,
-                                        Injection,
-                                        time_srs=time_slice[:3],
-                                        fluid_prop=Fluid,
-                                        fig=Fig_PS_M,
-                                        point1=ext_pnts[0],
-                                        point2=ext_pnts[1])
-
-
-ext_pnts = np.empty((2, 2), dtype=np.float64)
-Fig_PS_Mp = plot_fracture_list_slice(Fr_slice[3:],
-                                  variable='pn',
-                                  projection='2D',
-                                  plot_cell_center=True,
-                                  extreme_points=ext_pnts)
-# plot slice of width analytical
-Fig_PS_Mp = plot_analytical_solution_slice('Mp',
-                                        'pn',
-                                        Solid,
-                                        Injection,
-                                        time_srs=time_slice[3:],
-                                        fluid_prop=Fluid,
-                                        fig=Fig_PS_Mp,
-                                        point1=ext_pnts[0],
-                                        point2=ext_pnts[1])
+# # plot slice of width
+# time_slice = np.asarray([0.5, 5, 45, 5e4, 5e5, 5e6])
+# Fr_slice, properties = load_fractures(address="./Data/Mpulse",
+#                                       time_srs=time_slice)       # load specific fractures
+# time_slice = get_fracture_variable(Fr_slice,
+#                                    variable='time')
+#
+# ext_pnts = np.empty((2, 2), dtype=np.float64)
+# Fig_WS_M = plot_fracture_list_slice(Fr_slice[:3],
+#                                   variable='w',
+#                                   projection='2D',
+#                                   plot_cell_center=True,
+#                                   extreme_points=ext_pnts)
+# # plot slice of width analytical
+# Fig_WS_M = plot_analytical_solution_slice('M',
+#                                         'w',
+#                                         Solid,
+#                                         Injection,
+#                                         time_srs=time_slice[:3],
+#                                         fluid_prop=Fluid,
+#                                         fig=Fig_WS_M,
+#                                         point1=ext_pnts[0],
+#                                         point2=ext_pnts[1])
+#
+#
+# ext_pnts = np.empty((2, 2), dtype=np.float64)
+# Fig_WS_Mp = plot_fracture_list_slice(Fr_slice[3:],
+#                                   variable='w',
+#                                   projection='2D',
+#                                   plot_cell_center=True,
+#                                   extreme_points=ext_pnts)
+# # plot slice of width analytical
+# Fig_WS_Mp = plot_analytical_solution_slice('Mp',
+#                                         'w',
+#                                         Solid,
+#                                         Injection,
+#                                         time_srs=time_slice[3:],
+#                                         fluid_prop=Fluid,
+#                                         fig=Fig_WS_Mp,
+#                                         point1=ext_pnts[0],
+#                                         point2=ext_pnts[1])
 
 plt.show(block=True)
