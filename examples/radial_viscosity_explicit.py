7b8858314fc3059916d9616293fec0adaa3a905a
Andreas Möri <andreas.mori@epfl.ch>
2020-10-09 17:32:33 +0200

    Started the implementation of the batch run.

--- examples/radial_viscosity_explicit.py
+++ examples/radial_viscosity_explicit.py
@@ -13,6 +13,8 @@ from properties import MaterialProperties, FluidProperties, InjectionProperties,
 from fracture import Fracture
 from controller import Controller
 from fracture_initialization import Geometry, InitializationParameters
+import os
+
 
 # creating mesh
 Mesh = CartesianMesh(0.3, 0.3, 41, 41)
@@ -63,6 +65,8 @@ controller = Controller(Fr,
                         Injection,
                         simulProp)
 
+
+
 # run the simulation
 controller.run()
 
@@ -70,97 +74,99 @@ controller.run()
 # plotting results #
 ####################
 
-from visualization import *
 
-# loading simulation results
-Fr_list, properties = load_fractures(address="./Data/M_radial_explicit")        # load all fractures
-time_srs = get_fracture_variable(Fr_list, variable='time')                      # list of times
+if not os.path.isfile('./batch_run.txt'): # We only visualize for runs of specific examples
 
-# plot fracture radius
-plot_prop = PlotProperties()
-plot_prop.lineStyle = '.'               # setting the line style to point
-plot_prop.graphScaling = 'loglog'       # setting to log log plot
-Fig_R = plot_fracture_list(Fr_list,
-                           variable='d_mean',
-                           plot_prop=plot_prop)
-# plot analytical radius
-Fig_R = plot_analytical_solution(regime='M',
-                                 variable='d_mean',
-                                 mat_prop=Solid,
-                                 inj_prop=Injection,
-                                 fluid_prop=Fluid,
-                                 time_srs=time_srs,
-                                 fig=Fig_R)
+    from visualization import *
 
-# plot width at center
-Fig_w = plot_fracture_list_at_point(Fr_list,
-                                    variable='w',
-                                    plot_prop=plot_prop)
-# plot analytical width at center
-Fig_w = plot_analytical_solution_at_point('M',
-                                          'w',
-                                          Solid,
-                                          Injection,
-                                          fluid_prop=Fluid,
-                                          time_srs=time_srs,
-                                          fig=Fig_w)
+    # loading simulation results
+    Fr_list, properties = load_fractures(address="./Data/M_radial_explicit")        # load all fractures
+    time_srs = get_fracture_variable(Fr_list, variable='time')                      # list of times
 
+    # plot fracture radius
+    plot_prop = PlotProperties()
+    plot_prop.lineStyle = '.'               # setting the line style to point
+    plot_prop.graphScaling = 'loglog'       # setting to log log plot
+    Fig_R = plot_fracture_list(Fr_list,
+                               variable='d_mean',
+                               plot_prop=plot_prop)
+    # plot analytical radius
+    Fig_R = plot_analytical_solution(regime='M',
+                                     variable='d_mean',
+                                     mat_prop=Solid,
+                                     inj_prop=Injection,
+                                     fluid_prop=Fluid,
+                                     time_srs=time_srs,
+                                     fig=Fig_R)
 
-time_srs = np.asarray([2, 200, 5000, 30000, 100000])
-Fr_list, properties = load_fractures(address="./Data/M_radial_explicit",
-                                     time_srs=time_srs)
-time_srs = get_fracture_variable(Fr_list,
-                                 variable='time')
-
-# plot footprint
-Fig_FP = plot_fracture_list(Fr_list,
-                            variable='mesh',
-                            projection='2D')
-Fig_FP = plot_fracture_list(Fr_list,
-                            variable='footprint',
-                            projection='2D',
-                            fig=Fig_FP)
-# plot analytical footprint
-Fig_FP = plot_analytical_solution('M',
-                                  'footprint',
-                                  Solid,
-                                  Injection,
-                                  fluid_prop=Fluid,
-                                  time_srs=time_srs,
-                                  projection='2D',
-                                  fig=Fig_FP)
+    # plot width at center
+    Fig_w = plot_fracture_list_at_point(Fr_list,
+                                        variable='w',
+                                        plot_prop=plot_prop)
+    # plot analytical width at center
+    Fig_w = plot_analytical_solution_at_point('M',
+                                              'w',
+                                              Solid,
+                                              Injection,
+                                              fluid_prop=Fluid,
+                                              time_srs=time_srs,
+                                              fig=Fig_w)
 
 
-# plot slice
-ext_pnts = np.empty((2, 2), dtype=np.float64)
-Fig_WS = plot_fracture_list_slice(Fr_list,
-                                  variable='w',
-                                  projection='2D',
-                                  plot_cell_center=True,
-                                  extreme_points=ext_pnts)
-# plot slice analytical
-Fig_WS = plot_analytical_solution_slice('M',
-                                        'w',
-                                        Solid,
-                                        Injection,
-                                        fluid_prop=Fluid,
-                                        fig=Fig_WS,
-                                        time_srs=time_srs,
-                                        point1=ext_pnts[0],
-                                        point2=ext_pnts[1])
+    time_srs = np.asarray([2, 200, 5000, 30000, 100000])
+    Fr_list, properties = load_fractures(address="./Data/M_radial_explicit",
+                                         time_srs=time_srs)
+    time_srs = get_fracture_variable(Fr_list,
+                                     variable='time')
 
-#plotting in 3D
-Fig_Fr = plot_fracture_list(Fr_list,
-                            variable='mesh',
-                            projection='3D')
-Fig_Fr = plot_fracture_list(Fr_list,
-                            variable='width',
-                            projection='3D',
-                            fig=Fig_Fr)
-Fig_Fr = plot_fracture_list(Fr_list,
-                            variable='footprint',
-                            projection='3D',
-                            fig=Fig_Fr)
+    # plot footprint
+    Fig_FP = plot_fracture_list(Fr_list,
+                                variable='mesh',
+                                projection='2D')
+    Fig_FP = plot_fracture_list(Fr_list,
+                                variable='footprint',
+                                projection='2D',
+                                fig=Fig_FP)
+    # plot analytical footprint
+    Fig_FP = plot_analytical_solution('M',
+                                      'footprint',
+                                      Solid,
+                                      Injection,
+                                      fluid_prop=Fluid,
+                                      time_srs=time_srs,
+                                      projection='2D',
+                                      fig=Fig_FP)
 
-plt.show(block=True)
 
+    # plot slice
+    ext_pnts = np.empty((2, 2), dtype=np.float64)
+    Fig_WS = plot_fracture_list_slice(Fr_list,
+                                      variable='w',
+                                      projection='2D',
+                                      plot_cell_center=True,
+                                      extreme_points=ext_pnts)
+    # plot slice analytical
+    Fig_WS = plot_analytical_solution_slice('M',
+                                            'w',
+                                            Solid,
+                                            Injection,
+                                            fluid_prop=Fluid,
+                                            fig=Fig_WS,
+                                            time_srs=time_srs,
+                                            point1=ext_pnts[0],
+                                            point2=ext_pnts[1])
+
+    #plotting in 3D
+    Fig_Fr = plot_fracture_list(Fr_list,
+                                variable='mesh',
+                                projection='3D')
+    Fig_Fr = plot_fracture_list(Fr_list,
+                                variable='width',
+                                projection='3D',
+                                fig=Fig_Fr)
+    Fig_Fr = plot_fracture_list(Fr_list,
+                                variable='footprint',
+                                projection='3D',
+                                fig=Fig_Fr)
+
+    plt.show(block=True)
