d948aff6dc45877ce36f53504aa4e81bf4bb8546
carlo peruzzo <carlo.peruzzo@epfl.ch>
2020-10-12 11:06:53 +0200

    Commenting on the tip correction

--- src/time_stepping.py
+++ src/time_stepping.py
@@ -276,6 +276,15 @@ def injection_same_footprint(Fr_lstTmStp, C, timeStep, Qin, mat_properties, flui
     else:
         doublefracturedictionary = {"number_of_fronts": Fr_lstTmStp.fronts_dictionary['number_of_fronts']}
 
+    # todo: make tip correction while injecting in the same footprint
+    ##########################################################################################
+    #                                                                                        #
+    #  when we inject on the same footprint we should make the tip correction at the tip     #
+    #  this is not done, but it will not affect the accuracy, only the speed of convergence  #
+    #  since we are never accessing the diagonal of the elasticity matrix when iterating on  #
+    #  the position of the front.                                                            #
+    #                                                                                        #
+    ##########################################################################################
     w_k, p_k, return_data = solve_width_pressure(Fr_lstTmStp, #Fr_lstTmStp
                                          sim_properties,
                                          fluid_properties,
@@ -1059,6 +1068,16 @@ def solve_width_pressure(Fr_lstTmStp, sim_properties, fluid_properties, mat_prop
             EltTip_sym = Fr_lstTmStp.mesh.corresponding[EltTip]
             EltTip_sym = np.unique(EltTip_sym)
 
+            # todo: make tip correction while injecting in the same footprint
+            ##########################################################################################
+            #                                                                                        #
+            #  when we inject on the same footprint we should make the tip correction at the tip     #
+            #  this is not done, but it will not affect the accuracy, only the speed of convergence  #
+            #  since we are never accessing the diagonal of the elasticity matrix when iterating on  #
+            #  the position of the front.                                                            #
+            #                                                                                        #
+            ##########################################################################################
+
             # CARLO: we can remove it because the diagonal terms of C are never accessed
             # FillF_mesh = np.zeros((Fr_lstTmStp.mesh.NumberOfElts,), )
             # FillF_mesh[EltTip] = FillFrac
@@ -1110,6 +1129,15 @@ def solve_width_pressure(Fr_lstTmStp, sim_properties, fluid_properties, mat_prop
             # CARLO: we can remove it because the diagonal terms of C are never accessed
             # C[np.ix_(EltTip_sym[partlyFilledTip_sym], EltTip_sym[partlyFilledTip_sym])] = C_EltTip
         else:
+            # todo: make tip correction while injecting in the same footprint
+            ##########################################################################################
+            #                                                                                        #
+            #  when we inject on the same footprint we should make the tip correction at the tip     #
+            #  this is not done, but it will not affect the accuracy, only the speed of convergence  #
+            #  since we are never accessing the diagonal of the elasticity matrix when iterating on  #
+            #  the position of the front.                                                            #
+            #                                                                                        #
+            ##########################################################################################
             # CARLO: we can remove it because the diagonal terms of C are never accessed
             # C_EltTip = np.copy(C[np.ix_(EltTip[partlyFilledTip],
             #                             EltTip[partlyFilledTip])])  # keeping the tip element entries to restore current
