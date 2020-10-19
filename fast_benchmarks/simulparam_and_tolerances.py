9c64e38bfc955d1a12c32c1b213852ce01cfe334
carlo peruzzo <carlo.peruzzo@epfl.ch>
2020-10-19 10:44:41 +0200

    Improved the fast_benchmarks test suite

--- fast_benchmarks/simulparam_and_tolerances.py
+++ fast_benchmarks/simulparam_and_tolerances.py
@@ -6,7 +6,7 @@ Created by Carlo Peruzzo on 01.09.20.
 Copyright (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, Geo-Energy Laboratory, 2016-2020.
 All rights reserved. See the LICENSE.TXT file for more details.
 """
-import numpy as np
+
 testnames = dict(
     radial_M_explicit_newfront={'front_reconstruction': 'LS_continousfront',
                                 'front_advancement': 'explicit',
@@ -59,7 +59,7 @@ simulparam = dict(
                    'Nx': 41, 'Ny': 41,
                    'nu': 0.4, 'youngs_mod': 3.3e10, 'K_Ic': 1e6,
                    'Cl': 0.,  'Q0': 0.001,          'viscosity': 0.,
-                   'finalTime': 5e2, 'initialR': 0.15})
+                   'finalTime': 1e5, 'initialR': 0.15})
 
 
 
@@ -82,50 +82,49 @@ tolerances = dict(
 
     radial_M_implicit_newfront= { 'radius_toll':   [0.05],
                                   'w_center_toll': [0.05],
-                                  'w_section_toll_max_value': np.array([7. / 1000., 1.1 / 100., 1.7 / 100., 1.1 / 100., 1.8 / 100]),
-                                  'w_section_toll_cumulative_value': np.array([8. / 100., 14. / 100., 0.18, 0.2, 0.19]),
-                                  'p_section_toll_max_value': np.array([7. / 1000., 1.1 / 100., 1.7 / 100., 1.1 / 100., 1.8 / 100]),
-                                  'p_section_toll_cumulative_value': np.array([8. / 100., 14. / 100., 0.18, 0.2, 0.19])
+                                  'w_section_toll_max_value': [0.011, 0.015, 0.024, 0.02, 0.024],
+                                  'w_section_toll_cumulative_value': [0.12, 0.15, 0.24, 0.35, 0.28],
+                                  'p_section_toll_max_value': [0.32, 0.072, 0.0373, 0.091, 0.0092],
+                                  'p_section_toll_cumulative_value': [1.71, 0.36, 0.21, 0.27, 0.039]
                                   },
 
     radial_M_implicit_oldfront= { 'radius_toll':   [0.05],
                                   'w_center_toll': [0.05],
-                                  'w_section_toll_max_value': np.array([7. / 1000., 1.1 / 100., 1.7 / 100., 1.1 / 100., 1.8 / 100]),
-                                  'w_section_toll_cumulative_value': np.array([8. / 100., 14. / 100., 0.18, 0.2, 0.19]),
-                                  'p_section_toll_max_value': np.array([7. / 1000., 1.1 / 100., 1.7 / 100., 1.1 / 100., 1.8 / 100]),
-                                  'p_section_toll_cumulative_value': np.array([8. / 100., 14. / 100., 0.18, 0.2, 0.19])
+                                  'w_section_toll_max_value': [0.011, 0.012, 0.018, 0.011, 0.0249],
+                                  'w_section_toll_cumulative_value': [0.085, 0.13, 0.185, 0.187, 0.259],
+                                  'p_section_toll_max_value': [0.31, 0.072, 0.037, 0.026, 0.0092],
+                                  'p_section_toll_cumulative_value': [1.21, 0.295, 0.178, 0.097, 0.039]
                                   },
 
     radial_K_explicit_newfront= { 'radius_toll':   [0.05],
-                                  'w_center_toll': [0.05],
-                                  'w_section_toll_max_value': np.array([7. / 1000., 1.1 / 100., 1.7 / 100., 1.1 / 100., 1.8 / 100]),
-                                  'w_section_toll_cumulative_value': np.array([8. / 100., 14. / 100., 0.18, 0.2, 0.19]),
-                                  'p_section_toll_max_value': np.array([7. / 1000., 1.1 / 100., 1.7 / 100., 1.1 / 100., 1.8 / 100]),
-                                  'p_section_toll_cumulative_value': np.array([8. / 100., 14. / 100., 0.18, 0.2, 0.19])
+                                  'w_center_toll': [0.075],
+                                  'w_section_toll_max_value': [0.007541, 0.02500, 0.05338, 0.05069, 0.05767],
+                                  'w_section_toll_cumulative_value': [0.08947, 0.2514, 0.4729, 0.7319, 0.7217],
+                                  'p_section_toll_max_value': [0.4796, 0.2004, 0.1037, 0.07175, 0.05803],
+                                  'p_section_toll_cumulative_value': [6.592, 4.415, 2.480, 1.602, 0.6789]
                                   },
 
     radial_K_explicit_oldfront= { 'radius_toll':   [0.05],
-                                  'w_center_toll': [0.05],
-                                  'w_section_toll_max_value': np.array([7. / 1000., 1.1 / 100., 1.7 / 100., 1.1 / 100., 1.8 / 100]),
-                                  'w_section_toll_cumulative_value': np.array([8. / 100., 14. / 100., 0.18, 0.2, 0.19]),
-                                  'p_section_toll_max_value': np.array([7. / 1000., 1.1 / 100., 1.7 / 100., 1.1 / 100., 1.8 / 100]),
-                                  'p_section_toll_cumulative_value': np.array([8. / 100., 14. / 100., 0.18, 0.2, 0.19])
+                                  'w_center_toll': [0.078],
+                                  'w_section_toll_max_value': [0.007027, 0.02294, 0.05190, 0.05028, 0.05925],
+                                  'w_section_toll_cumulative_value': [0.08875, 0.2517, 0.4895, 0.6835, 0.7385],
+                                  'p_section_toll_max_value': [0.4796, 0.2004, 0.1037, 0.07175, 0.05803],
+                                  'p_section_toll_cumulative_value': [6.597, 4.427, 2.491, 1.602, 0.6814]
                                   },
 
-    radial_K_implicit_newfront= { 'radius_toll':   [0.05],
-                                  'w_center_toll': [0.05],
-                                  'w_section_toll_max_value': np.array([7. / 1000., 1.1 / 100., 1.7 / 100., 1.1 / 100., 1.8 / 100]),
-                                  'w_section_toll_cumulative_value': np.array([8. / 100., 14. / 100., 0.18, 0.2, 0.19]),
-                                  'p_section_toll_max_value': np.array([7. / 1000., 1.1 / 100., 1.7 / 100., 1.1 / 100., 1.8 / 100]),
-                                  'p_section_toll_cumulative_value': np.array([8. / 100., 14. / 100., 0.18, 0.2, 0.19])
+    radial_K_implicit_newfront= { 'radius_toll':   [0.074],
+                                  'w_center_toll': [0.21],
+                                  'w_section_toll_max_value': [0.01134, 0.01647, 0.09363, 0.1085, 0.2023],
+                                  'w_section_toll_cumulative_value': [0.1246, 0.2620, 1.2202, 1.440, 2.780],
+                                  'p_section_toll_max_value': [0.4752, 0.1930, 0.1029, 0.07285, 0.05803],
+                                  'p_section_toll_cumulative_value': [6.782, 3.902, 2.924, 1.982, 1.042]
                                   },
 
-    radial_K_implicit_oldfront= { 'radius_toll':   [0.05],
-                                  'w_center_toll': [0.05],
-                                  'w_section_toll_max_value': np.array([7. / 1000., 1.1 /100., 1.7/100., 1.1 /100., 1.8/100]),
-                                  'w_section_toll_cumulative_value': np.array([8. / 100.,  14./100.,  0.18,     0.2,       0.19]),
-                                  'p_section_toll_max_value': np.array([7. / 1000., 1.1 /100., 1.7/100., 1.1 /100., 1.8/100]),
-                                  'p_section_toll_cumulative_value': np.array([8. / 100.,  14./100.,  0.18,     0.2,       0.19])
+    radial_K_implicit_oldfront= { 'radius_toll':   [0.062],
+                                  'w_center_toll': [0.185],
+                                  'w_section_toll_max_value': [0.01609, 0.01351, 0.04430, 0.09152, 0.07827],
+                                  'w_section_toll_cumulative_value': [0.2904, 0.1846, 0.5634, 2.250, 0.9036],
+                                  'p_section_toll_max_value': [0.5016, 0.2011, 0.1055, 0.0733, 0.05803],
+                                  'p_section_toll_cumulative_value': [11.39, 4.346, 2.655, 0.8088, 0.6952]
 }
 )
-
