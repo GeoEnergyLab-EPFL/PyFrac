46cb88dca0dc495cb9bc7cfb770fbb866a4ac2c9
peruzzo <carlo.peruzzo@epfl.ch>
2021-04-30 10:45:00 +0200

    Added boundary effect in the sparse solver, introducing lgmres, explicitly coding the left preconditioning via GMRES, removing boundary effect mesh file, adding prints

--- src/boundary_effect_mesh.py
+++
@@ -1,6 +0,0 @@
-import json
-
-file = '/home/carlo/BigWhamLink/BigWhamLink/Examples/StaticCrackBenchmarks/boundary_effect_mesh.json'
-with open(file) as json_file:
-    boundarymesh = json.load(json_file)
-    print("json file loaded")
