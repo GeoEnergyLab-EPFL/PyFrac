80b6f0f06a880a48b6a96a9b44b756bea697f210
Andreas Möri <andreas.mori@epfl.ch>
2021-07-12 10:13:59 +0200

    Put ready to work on Zenodo. Also included the development to set the band with for the level-set.

--- src/time_step_solution.py
+++ src/time_step_solution.py
@@ -535,9 +535,11 @@ def injection_extended_footprint(w_k, Fr_lstTmStp, C, timeStep, Qin, mat_propert
         # region expected to have the front after propagation. The signed distance of the cells only in this region will
         # evaluated with the fast marching method to avoid unnecessary computation cost
         current_prefactor = sim_properties.get_time_step_prefactor(Fr_lstTmStp.time + timeStep)
-        front_region = np.where(abs(Fr_lstTmStp.sgndDist) < current_prefactor * 12.66 * (
-                Fr_lstTmStp.mesh.hx ** 2 + Fr_lstTmStp.mesh.hy ** 2) ** 0.5)[0]
-        #front_region = np.arange(Fr_lstTmStp.mesh.NumberOfElts)
+        # front_region = np.where(abs(Fr_lstTmStp.sgndDist) < current_prefactor * 12.66 * (
+        #         Fr_lstTmStp.mesh.hx ** 2 + Fr_lstTmStp.mesh.hy ** 2) ** 0.5)[0]
+        front_region = np.where(abs(Fr_lstTmStp.sgndDist) < current_prefactor * sim_properties.prefactorLevelSetBand * (
+            Fr_lstTmStp.mesh.hx ** 2 + Fr_lstTmStp.mesh.hy ** 2) ** 0.5)[0]
+
         # the search region outwards from the front position at last time step
         pstv_region = np.where(Fr_lstTmStp.sgndDist[front_region] >= -(Fr_lstTmStp.mesh.hx ** 2 +
                                                                        Fr_lstTmStp.mesh.hy ** 2) ** 0.5)[0]
@@ -1656,9 +1658,11 @@ def time_step_explicit_front(Fr_lstTmStp, C, timeStep, Qin, mat_properties, flui
     sgndDist_k[Fr_lstTmStp.EltTip] = Fr_lstTmStp.sgndDist[Fr_lstTmStp.EltTip] - (timeStep *
                                                                                  Fr_lstTmStp.v)
     current_prefactor = sim_properties.get_time_step_prefactor(Fr_lstTmStp.time + timeStep)
-    cell_diag = (Fr_lstTmStp.mesh.hx ** 2 + Fr_lstTmStp.mesh.hy ** 2) ** 0.5
-    expected_range = max(current_prefactor * 12.66 * cell_diag, 1.5 * cell_diag) # expected range of possible propagation
-    front_region = np.where(abs(Fr_lstTmStp.sgndDist) < expected_range)[0]
+    # cell_diag = (Fr_lstTmStp.mesh.hx ** 2 + Fr_lstTmStp.mesh.hy ** 2) ** 0.5
+    # expected_range = max(current_prefactor * 12.66 * cell_diag, 1.5 * cell_diag) # expected range of possible propagation
+    # front_region = np.where(abs(Fr_lstTmStp.sgndDist) < expected_range)[0]
+    front_region = np.where(abs(Fr_lstTmStp.sgndDist) < current_prefactor * sim_properties.prefactorLevelSetBand * (
+            Fr_lstTmStp.mesh.hx ** 2 + Fr_lstTmStp.mesh.hy ** 2) ** 0.5)[0]
     #front_region = np.arange(Fr_lstTmStp.mesh.NumberOfElts)
     # the search region outwards from the front position at last time step
     pstv_region = np.where(Fr_lstTmStp.sgndDist[front_region] >= -(Fr_lstTmStp.mesh.hx ** 2 +
@@ -2167,8 +2171,10 @@ def time_step_explicit_front(Fr_lstTmStp, C, timeStep, Qin, mat_properties, flui
         # region expected to have the front after propagation. The signed distance of the cells only in this region will
         # evaluated with the fast marching method to avoid unnecessary computation cost
         current_prefactor = sim_properties.get_time_step_prefactor(Fr_lstTmStp.time + timeStep)
-        front_region = np.where(abs(Fr_lstTmStp.sgndDist) < current_prefactor * 12.66 * (
-                Fr_lstTmStp.mesh.hx ** 2 + Fr_lstTmStp.mesh.hy ** 2) ** 0.5)[0]
+        # front_region = np.where(abs(Fr_lstTmStp.sgndDist) < current_prefactor * 12.66 * (
+        #         Fr_lstTmStp.mesh.hx ** 2 + Fr_lstTmStp.mesh.hy ** 2) ** 0.5)[0]
+        front_region = np.where(abs(Fr_lstTmStp.sgndDist) < current_prefactor * sim_properties.prefactorLevelSetBand * (
+            Fr_lstTmStp.mesh.hx ** 2 + Fr_lstTmStp.mesh.hy ** 2) ** 0.5)[0]
         #front_region = np.arange(Fr_lstTmStp.mesh.NumberOfElts)
 
         if not np.in1d(Fr_kplus1.EltTip, front_region).any():
@@ -2286,4 +2292,3 @@ def time_step_explicit_front(Fr_lstTmStp, C, timeStep, Qin, mat_properties, flui
 
     exitstatus = 1
     return exitstatus, Fr_kplus1
-
