28b2e2d755e79fcf859e984bc7d9dbb12aa544c8
carlo peruzzo <carlo.peruzzo@epfl.ch>
2021-07-16 08:44:36 +0200

    improving post processing and adding the export to json

--- examples/simplified_input.py
+++ examples/simplified_input.py
@@ -299,27 +299,161 @@ if post_process_the_results:
                                       orientation='horizontal',
                                       point1=[my_X, my_Y]
                                       )
-
-
-    # --  FIGURE 8) plot fracture fluid pressure along a segment cutting the fracture at given times -- #
-    # --  the segment is vertical and passing at (my_X, my_Y) -- #
-    # plot the solution every "tsJump" time steps
-    tsJump = 10
-    sol_at_selected_times = []
-    for i in np.arange(0, len(Fr_list), tsJump):
-        sol_at_selected_times.append(Fr_list[i])
-    my_X = 0.
-    my_Y = 0.
-    ext_pnts = np.empty((2, 2), dtype=np.float64)
-    Fig_PN = plot_fracture_list_slice(sol_at_selected_times,
-                                      variable='pf',
-                                      projection='2D',
-                                      plot_cell_center=True,
-                                      extreme_points=ext_pnts,
-                                      orientation='vertical',
-                                      point1=[my_X, my_Y]
-                                      )
     plt.show(block=True)
+
+
+
+##########################
+# exporting to json file #
+##########################
+from visualization import *
+from postprocess_fracture import append_to_json_file
+
+# 1) write general information to json
+# 2) write to json the coordinates of the points defining the fracture front at each time:
+# 3) get the fracture opening w(t) versus time (t) at a point of coordinates myX and myY
+# 4) get the fluid pressure p(t) versus time (t) at a point of coordinates myX and myY
+# 5) get w(y) along a vertical line passing through mypoint for different times
+# 6) get pf(x) along a horizontal line passing through mypoint for different times
+# 7) get w(x,y,t) and pf(x,y,t)
+
+export_results = True
+to_export = [1,2,3,4,5,6,7]
+
+if export_results:
+
+    # decide the names of the Json files:
+    myJsonName_1 = "./Data/Pyfrac_"+sim_name+"_export.json"
+
+    # load the results:
+    print("\n 1) loading results")
+    Fr_list, properties = load_fractures(address=save_folder, sim_name=sim_name, load_all=True)
+    # or Fr_list, properties = load_fractures(address=save_folder, sim_name=sim_name, time_srs=np.linspace(initial_time, final_time, steps))
+    Solid, Fluid, Injection, simulProp = properties
+    print(" <-- DONE\n")
+
+    # 1) write general information to json
+    if 1 in to_export:
+        print("\n 2) writing general info")
+        time_srs = get_fracture_variable(Fr_list, variable='time')
+        time_srs = np.asarray(time_srs)
+
+        simul_info = {'Eprime': Solid.Eprime,
+                      'max_KIc': Solid.K1c.max(),
+                      'min_KIc': Solid.K1c.min(),
+                      'max_Sigma0': Solid.SigmaO.max(),
+                      'min_Sigma0': Solid.SigmaO.min(),
+                      'viscosity': Fluid.viscosity,
+                      'total_injection_rate': Injection.injectionRate.max(),
+                      'sources_coordinates_lastFR': Fr_list[-1].mesh.CenterCoor[Injection.sourceElem].tolist(),
+                      't_max': time_srs.max(),
+                      't_min': time_srs.min()}
+        append_to_json_file(myJsonName_1, simul_info, 'append2keyASnewlist', key='simul_info',
+                            delete_existing_filename=True)  # be careful: delete_existing_filename=True only the first time you call "append_to_json_file"
+
+    # 2) write to json the coordinates of the points defining the fracture front at each time:
+    if 2 in to_export:
+        print("\n 2) writing fronts")
+        time_srs = get_fracture_variable(Fr_list,variable='time') # get the list of times corresponding to each fracture object
+        append_to_json_file(myJsonName_1, time_srs, 'append2keyASnewlist', key='time_srs_of_Fr_list')
+        fracture_fronts = []
+        numberof_fronts = [] #there might be multiple fracture fronts in general
+        mesh_info = [] # if you do not make remeshing or mesh extension you can export it only once
+        index = 0
+        for fracture in Fr_list:
+            fracture_fronts.append(np.ndarray.tolist(fracture.Ffront))
+            numberof_fronts.append(fracture.number_of_fronts)
+            mesh_info.append([Fr_list[index].mesh.Lx, Fr_list[index].mesh.Ly, Fr_list[index].mesh.nx, Fr_list[index].mesh.ny])
+            index = index + 1
+        append_to_json_file(myJsonName_1, fracture_fronts, 'append2keyASnewlist', key='Fr_list')
+        append_to_json_file(myJsonName_1, numberof_fronts, 'append2keyASnewlist', key='Number_of_fronts')
+        append_to_json_file(myJsonName_1,mesh_info,'append2keyASnewlist', key='mesh_info')
+        print(" <-- DONE\n")
+
+    # 3) get the fracture opening w(t) versus time (t) at a point of coordinates myX and myY
+    if 3 in to_export:
+        print("\n 3) get w(t) at a point... ")
+        my_X = 0.02 ; my_Y = 0.
+        w_at_my_point, time_list_at_my_point = get_fracture_variable_at_point(Fr_list, variable='w', point=[my_X, my_Y])
+        append_to_json_file(myJsonName_1, w_at_my_point, 'append2keyASnewlist', key='w_at_my_point')
+        append_to_json_file(myJsonName_1, time_list_at_my_point, 'append2keyASnewlist', key='time_list_W_at_my_point')
+        print(" <-- DONE\n")
+
+
+
+    # 4) get the fluid pressure p(t) versus time (t) at a point of coordinates myX and myY
+    if 4 in to_export:
+        print("\n 4) get pf(t) at a point... ")
+        my_X = 0.02 ; my_Y = 0.
+        pf_at_my_point, time_list_at_my_point = get_fracture_variable_at_point(Fr_list, variable='pf', point=[my_X, my_Y])
+        append_to_json_file(myJsonName_1, pf_at_my_point, 'append2keyASnewlist', key='pf_at_my_point_A')
+        append_to_json_file(myJsonName_1, time_list_at_my_point, 'append2keyASnewlist', key='time_list_pf_at_my_point_A')
+        print(" <-- DONE\n")
+
+
+    # 5) get w(y) along a vertical line passing through mypoint for different times
+    if 5 in to_export:
+        print("\n 5) get w(y) with y passing through a specific point for different times... ")
+        my_X = 0.; my_Y = 0.
+        ext_pnts = np.empty((2, 2), dtype=np.float64)
+        fracture_list_slice = plot_fracture_list_slice(Fr_list,
+                                                       variable='w',
+                                                       projection='2D',
+                                                       plot_cell_center=True,
+                                                       extreme_points=ext_pnts,
+                                                       orientation='horizontal',
+                                                       point1=[my_X , my_Y],
+                                                       export2Json=True,
+                                                       export2Json_assuming_no_remeshing=False)
+        towrite = {'w_vert_slice_': fracture_list_slice}
+        append_to_json_file(myJsonName_1, towrite, 'extend_dictionary')
+        print(" <-- DONE\n")
+
+
+
+    # 6) get pf(x) along a horizontal line passing through mypoint for different times
+    if 6 in to_export:
+        print("\n 6) get pf(x) with x passing through a specific point for different times... ")
+        my_X = 0.; my_Y = 0.
+        ext_pnts = np.empty((2, 2), dtype=np.float64)
+        fracture_list_slice = plot_fracture_list_slice(Fr_list,
+                                                       variable='pf',
+                                                       projection='2D',
+                                                       plot_cell_center=True,
+                                                       extreme_points=ext_pnts,
+                                                       orientation='horizontal',
+                                                       point1=[my_X , my_Y],
+                                                       export2Json=True,
+                                                       export2Json_assuming_no_remeshing=False)
+        towrite = {'pf_horiz_slice_': fracture_list_slice}
+        append_to_json_file(myJsonName_1, towrite, 'extend_dictionary')
+        print(" <-- DONE\n")
+
+
+
+    # 7) get w(x,y,t) and pf(x,y,t)
+    if 7 in to_export:
+        print("\n 7) get w(x,y,t) and  pf(x,y,t)... ")
+        wofxyandt = []
+        pofxyandt = []
+        info = []
+        jump = True #this is used to jump the first fracture
+        for frac in Fr_list:
+            if not jump:
+                wofxyandt.append(np.ndarray.tolist(frac.w))
+                pofxyandt.append(np.ndarray.tolist(frac.pFluid))
+                info.append([frac.mesh.Lx,frac.mesh.Ly,frac.mesh.nx,frac.mesh.ny,frac.time])
+            else:
+                jump = False
+
+        append_to_json_file(myJsonName_1, wofxyandt, 'append2keyASnewlist', key='w')
+        append_to_json_file(myJsonName_1, pofxyandt, 'append2keyASnewlist', key='p')
+        append_to_json_file(myJsonName_1, info, 'append2keyASnewlist', key='info_for_w_and_p')
+        print(" <-- DONE\n")
+
+    print("DONE! in " + myJsonName_1)
+    plt.show(block=True)
+
 
 
 # -------------- END OF FILE -------------- #
