fixing some bugs and bad behaviours, i.e. computation of the band where the front is expected, angle in front reconstruction.

src/continuous_front_reconstruction.py
@@ -435,7 +435,7 @@ def plot_final_reconstruction(mesh,
     plt.plot(mesh.CenterCoor[oldRibbon,0]*1.05, mesh.CenterCoor[oldRibbon,1], '.',color='green')
     plt.plot(mesh.CenterCoor[listofTIPcells, 0] + mesh.hx / 10, mesh.CenterCoor[listofTIPcells, 1] + mesh.hy / 10, '.', color='blue')
 
-def plot_xy_points(anularegion, mesh, sgndDist_k, Ribbon, x,y, fig=None, annotate_cellName=False,annotate_edgeName=False, annotatePoints=True, grid=True, oldfront=None):
+def plot_xy_points(anularegion, mesh, sgndDist_k, Ribbon, x,y, fig=None, annotate_cellName=False,annotate_edgeName=False, annotatePoints=True, grid=True, oldfront=None, joinPoints = True, disregard_plus=False):
         #fig = None
         if fig is None:
             fig = plt.figure()
@@ -450,13 +450,17 @@ def plot_xy_points(anularegion, mesh, sgndDist_k, Ribbon, x,y, fig=None, annotat
         nonRibbon = np.setdiff1d(anularegion, Ribbon)
         Positive_nonRibbon = nonRibbon[np.where(sgndDist_k[nonRibbon] > 0)[0]]
         plt.plot(mesh.CenterCoor[Ribbon, 0], mesh.CenterCoor[Ribbon, 1], ".", marker="_", color='g')
-        plt.plot(mesh.CenterCoor[Positive_nonRibbon, 0], mesh.CenterCoor[Positive_nonRibbon, 1], ".", marker="+",
-                 color='r')
+        if not disregard_plus:
+            plt.plot(mesh.CenterCoor[Positive_nonRibbon, 0], mesh.CenterCoor[Positive_nonRibbon, 1], ".", marker="+",
+                     color='r')
         Negative_nonRibbon = np.setdiff1d(nonRibbon, Positive_nonRibbon)
         if Negative_nonRibbon.size > 0:  # plot them
             plt.plot(mesh.CenterCoor[Negative_nonRibbon, 0], mesh.CenterCoor[Negative_nonRibbon, 1], ".",
                      marker="_", color='b')
-        plt.plot(np.asarray(x), np.asarray(y), '.-', color='red')
+        if joinPoints:
+            plt.plot(np.asarray(x), np.asarray(y), '.-', color='red')
+        else:
+            plt.plot(np.asarray(x), np.asarray(y), '.', color='red')
 
         if annotate_cellName:
             x_center = mesh.CenterCoor[anularegion,0]
@@ -3178,7 +3182,7 @@ def reconstruct_front_continuous(sgndDist_k, anularegion, Ribbon, eltsChannel, m
                     if dLSdy == 0. and dLSdx != 0.:
                         fullyfractured_angle.append(0.)
                     elif dLSdy != 0. and dLSdx == 0:
-                        fullyfractured_angle.append(np.pi)
+                        fullyfractured_angle.append(np.pi/2.)
                     elif dLSdy != 0. and dLSdx != 0:
                         fullyfractured_angle.append(np.arctan(np.abs(dLSdy) / np.abs(dLSdx)))
                     else:
@@ -3469,3 +3473,11 @@ def you_advance_more_than_2_cells(fully_traversed_k,EltTip_last_tmstp,NeiElement
     else: #==0
         return False
 
+def get_xy_from_Ffront(Ffront):
+    x = []
+    y = []
+    for segment in Ffront:
+        [xa,ya,xb,yb] = segment
+        x.append(xa)
+        y.append(ya)
+    return x, y

src/level_set.py
@@ -497,7 +497,18 @@ def get_cells_inside_circle(mesh, r, center):
 
 
 
-def  get_front_region(mesh, EltRibbon, sgndDist_k_EltRibbon, EltChannel_lstTmStp):
+def  get_front_region(mesh, EltRibbon, sgndDist_k_EltRibbon):
+    """
+    This function returns a list of elements that form a band where the location of the tip is expected to be.
+    Args:
+        mesh:
+        EltRibbon:
+        sgndDist_k_EltRibbon:
+        EltChannel_lstTmStp:
+
+    Returns:
+
+    """
     front_region = []
     advancing_fast = []
     # take the cells in a circle drown from each ribbon cell with radius equal to the distance to the front from the tip inversion
@@ -507,29 +518,11 @@ def  get_front_region(mesh, EltRibbon, sgndDist_k_EltRibbon, EltChannel_lstTmStp
             radius_i = np.abs(sgndDist_k_EltRibbon[i]) + 2.5 * mesh.cellDiag
             advancing_fast.append(cell_i)
         else:
-            radius_i = np.abs(sgndDist_k_EltRibbon[i]) + mesh.cellDiag
+            radius_i = np.abs(sgndDist_k_EltRibbon[i]) + 1.5 * mesh.cellDiag
         center_i = mesh.CenterCoor[cell_i]
         new_cells = get_cells_inside_circle(mesh, radius_i, center_i)
         for j in new_cells:
             front_region.append(j)
-    front_region = np.unique(front_region)
-
-    # remove the cells inside the crack at the previous time step
-    front_region = np.setdiff1d(front_region, EltChannel_lstTmStp)
-
-    # add 1 row of cells behind the ribbon
-    reducedEltRibbon = np.setdiff1d(EltRibbon,advancing_fast)
-    new_elem = np.unique(mesh.NeiElements[reducedEltRibbon].flatten())
-    front_region = np.concatenate((front_region, new_elem))
-
-    # add another row of cells behind the ribbon
-    new_elem = np.unique(mesh.NeiElements[new_elem].flatten())
-    front_region = np.concatenate((front_region, new_elem))
-
-    # # add another row of cells behind the ribbon
-    # new_elem = np.unique(mesh.NeiElements[new_elem].flatten())
-    # front_region = np.concatenate((front_region, new_elem))
-
     front_region = np.unique(front_region)
 
     # take out the ribbon