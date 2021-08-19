Fixing a bug in the computation of the tip volume. Adapting the convergence criteria on the front position. Fixing the locate elem. func.. Adding remeshing for Hmat or toeplitz with Gmres. Adding some plots. Fixing some typos.

src/controller.py
@@ -21,6 +21,7 @@ from properties import LabelProperties, IterationProperties, PlotProperties
 from properties import instrument_start, instrument_close
 from elasticity import load_isotropic_elasticity_matrix, load_TI_elasticity_matrix, mapping_old_indexes
 from elasticity import load_isotropic_elasticity_matrix_toepliz
+from Hdot import Hdot_3DR0opening
 from mesh import CartesianMesh
 from time_step_solution import attempt_time_step
 from visualization import plot_footprint_analytical, plot_analytical_solution,\
@@ -1337,7 +1338,7 @@ class Controller:
         self.injection_prop.remesh(coarse_mesh, self.fracture.mesh)
 
         # We adapt the elasticity matrix
-        if not self.sim_prop.useBlockToeplizCompression:
+        if not self.sim_prop.useBlockToeplizCompression and not self.sim_prop.useHmat:
             if direction is None:
                 if rem_factor == self.sim_prop.remeshFactor:
                     self.C *= 1 / self.sim_prop.remeshFactor
@@ -1358,12 +1359,47 @@ class Controller:
                 #rem_factor = 10
                 log.info("Extending the elasticity matrix...")
                 self.extend_isotropic_elasticity_matrix(coarse_mesh, direction=direction)
-        else:
+        elif self.sim_prop.useBlockToeplizCompression and not self.sim_prop.useHmat:
             # if direction is None:
             #     rem_factor = self.sim_prop.remeshFactor
             # else:
             #     rem_factor = 10
             self.C.reload(coarse_mesh)
+        elif not self.sim_prop.useBlockToeplizCompression and self.sim_prop.useHmat:
+            max_leaf_size = self.C.max_leaf_size
+            eta = self.C.eta
+            eps_aca = self.C.eps_aca
+            self.C = Hdot_3DR0opening()
+            try:
+                properties = [self.solid_prop.youngs_mod, self.solid_prop.nu]
+            except:
+                log.info("Variable youngs_mod is not defined")
+                log.info("Enter your value for the Young mod (use [Pa] if you do not know): ")
+                self.solid_prop.youngs_mod = input("youngs_mod value? : ")
+                self.solid_prop.youngs_mod = float(self.solid_prop.youngs_mod)
+                log.info("Enter your value for the Poisson's ratio: ")
+                self.solid_prop.nu = input("Poisson's ratio value? : ")
+                self.solid_prop.nu = float(self.solid_prop.nu)
+                #check
+                Eprime = self.solid_prop.youngs_mod / (1 - self.solid_prop.nu ** 2)
+                if np.abs(Eprime - self.solid_prop.Eprime)/ self.solid_prop.Eprime > 0.02:
+                    message = "Either youngs_mod or nu are wrong and E'=E/(1-nu^2) is >2% different from the one known"
+                    log.error(message)
+                    raise SystemExit(message)
+                properties = [self.solid_prop.youngs_mod, self.solid_prop.nu]
+            data = [max_leaf_size, eta, eps_aca, properties, coarse_mesh.VertexCoor, coarse_mesh.Connectivity, coarse_mesh.hx, coarse_mesh.hy]
+            log.info("Building C form Hmat...\n")
+            log.info("this might take some time...\n")
+            begtime_HMAT = time.time()
+            self.C.set(data)
+            endtime_HMAT = time.time()
+            compute_HMAT = endtime_HMAT - begtime_HMAT
+            log.info("Hmat time : "+ str(compute_HMAT))
+        else:
+            #use toeplitz
+            log.info("Using isotropic elasticity matrix with Block Toepliz Compression")
+            self.C = load_isotropic_elasticity_matrix_toepliz(coarse_mesh, self.solid_prop.Eprime)
+            self.sim_prop.useBlockToeplizCompression = True
 
         self.fracture = self.fracture.remesh(rem_factor,
                                              self.C,

src/time_step_solution.py
@@ -241,26 +241,36 @@ def attempt_time_step(Frac, C, Boundary, mat_properties, fluid_properties, sim_p
                 log.debug( 'Norm of subsequent Norms of subsequent filling fraction estimates = ' + str(abs((previous_norm-norm)/norm)) + ' < 0.0001')
                 exitstatus = 15
                 return exitstatus, None
-            else:
-                previous_norm = norm
 
         # preventing infinite or not effective loops
         if (k >= sim_properties.maxFrontItrs and norm > 0.026) or k > 100:
-            exitstatus = 6
-            return exitstatus, None
+            if norm > 10*sim_properties.tolFractFront or  k>200:
+                exitstatus = 6
+                return exitstatus, None
 
         if norm < sim_properties.tolFractFront and loop_already_forced:
-            loop_already_forced = False
-            force_1loop = False
+            if norm < previous_norm:
+                # the convergence has been achieved
+                loop_already_forced = False
+                force_1loop = False
+            else:
+                # forcing another loop
+                loop_already_forced = True
+                force_1loop = True
         elif norm < sim_properties.tolFractFront and not loop_already_forced:
+            log.debug(' --> Forcing one more loop to see if the convergence has been really achieved')
             loop_already_forced = True
             force_1loop = True
         elif norm >= sim_properties.tolFractFront and loop_already_forced:
+            log.debug(' --> convergence not achieved, iterate more if other conditions allows for that')
             loop_already_forced = False
             force_1loop = False
         elif norm >= sim_properties.tolFractFront and not loop_already_forced:
+            # normal case
             loop_already_forced = False
             force_1loop = False
+
+        previous_norm = norm
 
     # check if we advanced more than two cells
     if exitstatus == 1:
@@ -753,7 +763,7 @@ def injection_extended_footprint(w_k, Fr_lstTmStp, C, Boundary, timeStep, Qin, m
     # x,y = get_xy_from_Ffront(Ffront)
     # plot_xy_points(front_region, Fr_lstTmStp.mesh, sgndDist_k, Fr_lstTmStp.EltRibbon, x, y, fig=None, annotate_cellName=False,
     #                annotate_edgeName=False, annotatePoints=False, grid=True, oldfront=front_previous_iter, joinPoints=True,
-    #                disregard_plus=False)
+    #                disregard_plus=False) # or oldfront=Fr_lstTmStp.Ffront
 
     if not np.in1d(EltsTipNew, front_region).any():
         raise SystemExit("The tip elements are not in the band. Increase the size of the band for FMM to evaluate"
@@ -881,27 +891,13 @@ def injection_extended_footprint(w_k, Fr_lstTmStp, C, Boundary, timeStep, Qin, m
             else:
                 Kprime_tip = (32. / np.pi) ** 0.5 * get_toughness_from_Front(Ffront,
                                                                                EltsTipNew,
+                                                                               EltTip_k,
                                                                                fully_traversed_k,
                                                                                Fr_lstTmStp.mesh,
                                                                                mat_properties,
-                                                                               alpha_k)
+                                                                               alpha_k,
+                                                                               get_from_mid_front = False)
 
-                # Kprime_tip = (32. / np.pi) ** 0.5 * get_toughness_from_midFront(Ffront,
-                #                                                                EltsTipNew,
-                #                                                                EltTip_k,
-                #                                                                fully_traversed_k,
-                #                                                                Fr_lstTmStp.mesh,
-                #                                                                mat_properties,
-                #                                                                alpha_k)
-
-                # # Kprime_tip = (32 / np.pi) ** 0.5 * get_toughness_from_midFront_zv(Ffront,
-                #                                                                EltsTipNew,
-                #                                                                fully_traversed_k,
-                #                                                                Fr_lstTmStp.mesh,
-                #                                                                mat_properties,
-                #                                                                alpha_k,
-                #                                                                l_k,
-                #                                                                zrVrtx_newTip)
 
     # from utility import plot_as_matrix
     # K = np.zeros((Fr_lstTmStp.mesh.NumberOfElts,), )
@@ -927,6 +923,11 @@ def injection_extended_footprint(w_k, Fr_lstTmStp, C, Boundary, timeStep, Qin, m
     #     log.warning("Stagnant front is only supported with universal tip asymptote. continuing...")
     #     stagnant = np.full((EltsTipNew.size,), False, dtype=bool)
 
+    # from utility import plot_as_matrix
+    # K = np.zeros((Fr_lstTmStp.mesh.NumberOfElts,), )
+    # K[EltsTipNew[np.where(stagnant)[0]]] = 2
+    # plot_as_matrix(K, Fr_lstTmStp.mesh)
+    # EltsTipNew[np.where(stagnant)[0]]
     if perfNode is not None:
         perfNode_tipWidth = instrument_start('tip width', perfNode)
         #todo close tip width instrumentation