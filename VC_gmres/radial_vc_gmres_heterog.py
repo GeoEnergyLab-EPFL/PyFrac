Fixing a bug in the computation of the tip volume. Adapting the convergence criteria on the front position. Fixing the locate elem. func.. Adding remeshing for Hmat or toeplitz with Gmres. Adding some plots. Fixing some typos.

VC_gmres/radial_vc_gmres_heterog.py
@@ -60,7 +60,6 @@ def get_info(Fr_list_A):  # get L(t) and x_max(t) and p(t)
 import os
 import time
 import numpy as np
-from datetime import datetime
 
 # local imports
 from mesh import CartesianMesh
@@ -81,27 +80,34 @@ setup_logging_to_console(verbosity_level='debug')
 ########## OPTIONS #########
 run = True
 #run = False
-
+#----
+run_dir ="./Data/1p9_Hmat"
+#run_dir =  "./Data/noHmat"
+#run_dir =  "./Data/noHmat_coarse_tol"
+#run_dir =  "./Data/noHmat_DisTime"
+#run_dir =  "./Data/noHmat_DisTime_fastQ" #5 times Q and not time table prescribed
+#----
 restart = False
 #restart = True
-
+#----
+#use_HMAT = True
+use_HMAT = False
+#----
+use_direct_TOEPLITZ = True
+output_fol_B  = run_dir
+output_fol = run_dir
+#output_fol_B  = "./Data/temp"
+#output_fol = "./Data/temp"
+#output_fol ="./Data/noHmat_coarse_tol"
+output_fol = "./Data/noHmat_coarse_tol"
+output_fol_B  = "./Data/noHmat_coarse_tol"
+#----
 use_iterative = True
-
-use_HMAT = True
-#use_HMAT = False
-use_direct_TOEPLITZ = False
-
-output_fol = "./Data/radial_VC_gmres"
-#output_fol_B = "./Data/radial_VC_gmres"
-#output_fol = "./Data/radial_VC_gmres_res"
-output_fol_B = "./Data/radial_VC_gmres_res"
-ftPntJUMP = 10
+ftPntJUMP = 1
 ############################
-#print("NO SMOOTHING")
 
 if run:
     # creating mesh
-    #Mesh = CartesianMesh(3, 18, 151, 901)
     Mesh = CartesianMesh(2.7, 17.55, 91, 585)
 
     # solid properties
@@ -110,7 +116,6 @@ if run:
     Eprime = youngs_mod / (1 - nu ** 2)  # plain strain modulus
     properties = [youngs_mod, nu]
 
-
     def smoothing(K1, K2, r, delta, x):
         # instead of having -10/10, take the MESHNAME.Ly/Lx (if mesh square)
         #### LINEAR - DIRAC DELTA ####
@@ -118,9 +123,6 @@ if run:
         if  x < r-delta :
             return K1
         elif x >= r-delta and x<r :
-            # 0.0 restart long
-            # 0.3 test 1
-            # 0.1 test 2
             K12 = K1 + (K2-K1)*0.1
             a = (K12 - K1) / (delta)
             b = K1 - a * (r - delta)
@@ -129,7 +131,6 @@ if run:
             return K2
         else:
             print("ERROR")
-
 
         #### LINEAR ####
         # x = np.abs(x)
@@ -144,13 +145,13 @@ if run:
         # else:
         #     print("ERROR")
 
-
         #### EXPONENTIAL ####
         # x_b = 4000*(np.abs(x)-r)
         # if x >= 0.:
         #     return K1 + np.exp(x_b)/(np.exp(x_b)+1)*(K2-K1)
         # else:
         #     return K1 + np.exp(x_b)/(np.exp(x_b)+1)*(K2-K1)
+
         #### QUADRATIC ####
         # x = np.abs(x)
         # if x <= r:
@@ -164,12 +165,7 @@ if run:
         """ The function providing the toughness"""
         K_Ic = 0.5e6  # fracture toughness
         r = 1.48
-        #delta = 0.008
         delta = 0.001
-        # if np.abs(x) > r:
-        #     return 2.*K_Ic
-        # else:
-        #     return K_Ic
         return smoothing(K_Ic, 1.9*K_Ic, r, delta, x)
 
     # plot x_max vs time
@@ -219,40 +215,6 @@ if run:
 
     def sigmaO_func(x, y):
         return 0
-        # """ The function providing the stress"""
-        # if (np.floor(abs(y)) % 3) > 1 and abs(y) >0.:
-        #     return 48e6
-        # else:
-        #     return 36e6
-        # # comment the following section if you would like to consider field of stress
-        # # caracterized by the presence of less heterogeneities.
-        # lx = 0.20
-        # ly = 0.20
-        # if math.trunc(abs(x) / lx) >0:
-        #     if math.trunc(abs(x) / lx) %2 == 0:
-        #         x = abs(x) - (math.trunc(abs(x) / lx)) * lx
-        #     else :
-        #         x = abs(x) - (math.trunc(abs(x) / lx) + 1) * lx
-        #
-        # if math.trunc(abs(y) / ly) > 0:
-        #     if math.trunc(abs(y) / ly) %2 == 0:
-        #         y = abs(y) - (math.trunc(abs(y) / ly)) * ly
-        #     else :
-        #         y = abs(y) - (math.trunc(abs(y) / ly)+1) * ly
-        # # comment up to here
-        #
-        #
-        # """ The function providing the confining stress"""
-        # R=0.05
-        # x1=0.
-        # y1=0.2
-        #
-        # if (abs(x)-x1)**2+(abs(y)-y1)**2 < R**2:
-        #    return 60.0e6
-        # if (abs(x)-y1)**2+(abs(y)-x1)**2 < R**2:
-        #    return 60.0e6
-        # else:
-        #    return 5.0e6
 
     Solid = MaterialProperties(Mesh,
                               Eprime,
@@ -273,15 +235,15 @@ if run:
             C.set(data)
             endtime_HMAT = time.time()
             compute_HMAT = endtime_HMAT - begtime_HMAT
-            append_new_line('./Data/radial_VC_gmres/building_HMAT.txt', str(compute_HMAT))
-            print("Compression Ratio of the HMAT : ", C.compressionratio)
-
+            #append_new_line('./Data/radial_VC_gmres/building_HMAT.txt', str(compute_HMAT))
+            #print("Compression Ratio of the HMAT : ", C.compressionratio)
         else:
             from elasticity import load_isotropic_elasticity_matrix_toepliz
             C = load_isotropic_elasticity_matrix_toepliz(Mesh, Eprime)
 
     # injection parameters
     Q0 = 0.001
+    Q0 = 5*0.001
     Injection = InjectionProperties(Q0, Mesh)
 
     # fluid properties
@@ -293,20 +255,29 @@ if run:
     simulProp.tmStpPrefactor = 0.9  # decrease the pre-factor due to explicit front tracking
     simulProp.gmres_tol = 1e-15
     simulProp.saveToDisk = True
-    simulProp.tolFractFront = 0.00005
+    #simulProp.tolFractFront = 0.0005
+    simulProp.tolFractFront = 0.0001
     simulProp.plotTSJump = 1
     simulProp.set_volumeControl(True)
     if use_iterative: simulProp.volumeControlGMRES = True
     simulProp.bckColor = 'K1c'
-    simulProp.set_outputFolder("./Data/radial_VC_gmres")  # the disk address where the files are saved
+    simulProp.set_outputFolder(run_dir)   # the disk address where the files are saved
     simulProp.set_tipAsymptote('K')  # the tip asymptote is evaluated with the toughness dominated assumption
     simulProp.plotVar = ['footprint', 'custom']#,'regime']
     simulProp.frontAdvancing = 'implicit'  # <--- mandatory use
     simulProp.projMethod = 'LS_continousfront'  # <--- mandatory use
     #simulProp.set_solTimeSeries(np.arange(0., 3.7, 0.0025))
-    simulProp.force_time_schedule = True
-    simulProp.set_solTimeSeries(np.concatenate((np.arange(0., 1.30, 0.0085),
-                                                np.arange(1.30,4.00,0.01),)))
+    #simulProp.force_time_schedule = True
+    # simulProp.set_solTimeSeries(np.concatenate((np.arange(0., 1.30, 0.0085),
+    #                                             np.arange(1.30,4.00,0.01),)))
+    myrange = np.arange(0., 4., 0.0085)
+    t = 0.
+    allT =[0.]
+    while t < 4.:
+        t = t + 0.0075 * (1 + 0.6 * t)
+        allT.append(t)
+    #simulProp.set_solTimeSeries(np.asarray(allT))
+
     #simulProp.send_phone_msg = True
 
     # setting up mesh extension options
@@ -348,39 +319,30 @@ if run:
     # ################################################################################
     if restart:
         from visualization import *
-        Fr_list, properties = load_fractures(address="./Data/radial_VC_gmres", step_size=10)       # load all fractures                                                # list of times
+        Fr_list, properties = load_fractures(address=run_dir, step_size=10)       # load all fractures                                                # list of times
         Solid, Fluid, Injection, simulProp = properties
-        simulProp.set_tipAsymptote('K')
-        #simulProp.plotTSJump = 100
         Fr = Fr_list[-1]
-        simulProp.set_solTimeSeries(np.concatenate((np.arange(0., 1.30, 0.0085),
-                                                    np.arange(1.30, 4.00, 0.01),)))
-        # simulProp.set_solTimeSeries(np.concatenate((np.arange(0., 0.44, 0.009),
-        #                                             np.arange(0.44, 0.504, 0.008),
-        #                                             np.arange(0.504, 0.599, 0.006),
-        #                                             np.arange(0.599, 0.6546, 0.006),
-        #                                             np.arange(0.6546,6, 0.006))))
-        simulProp.force_time_schedule =True
-        #simulProp.tolFractFront = 0.0002
-        #simulProp.plotTSJump = 100
+        simulProp.set_outputFolder(run_dir)
+        simulProp.set_solTimeSeries(np.concatenate((np.arange(0., 1.0965, 0.0085),
+                                                    np.arange(1.0965, 7.9965, 0.025),)))
+        # simulProp.set_solTimeSeries(np.arange(0.,5., 0.0085))
         Solid = MaterialProperties(Mesh,
                                   Eprime,
                                   K1c_func=K1c_func,
                                   confining_stress_func = sigmaO_func,
                                   confining_stress=0.,
                                   minimum_width=0.)
-        simulProp.send_phone_msg = True
-
+        #simulProp.send_phone_msg = True
+        simulProp.useHmat=True
+        Solid.youngs_mod=3.3e10
+        Solid.nu=0.4
+        #simulProp.tolFractFront = 0.0001
         # setting up mesh extension options
         simulProp.meshExtensionAllDir = False
         simulProp.set_mesh_extension_factor(1.5)
         simulProp.set_mesh_extension_direction(['vertical'])
         simulProp.meshReductionPossible = False
         simulProp.simID = 'K1/K2=1.9' # do not use _
-
-
-        #from level_set import get_front_region
-        #Fr.front_region = get_front_region(Fr.mesh, Fr.EltRibbon, Fr.sgndDist[Fr.EltRibbon])`
     #############################################################################
 
 
@@ -417,10 +379,13 @@ if not os.path.isfile('./batch_run.txt'):  # We only visualize for runs of speci
 
     # plot fracture radius
     my_list = []
+    mytime = 10.4
     for i in np.arange(0,len(Fr_list_A),ftPntJUMP):
-        my_list.append(Fr_list_A[i])
+        if Fr_list_A[i].time < mytime:
+            my_list.append(Fr_list_A[i])
     for i in np.arange(0, len(Fr_list_B), ftPntJUMP):
-        my_list.append(Fr_list_B[i])
+        if Fr_list_B[i].time < mytime:
+            my_list.append(Fr_list_B[i])
     plot_prop = PlotProperties()
     Fig_R = plot_fracture_list(my_list,
                                variable='footprint',
@@ -552,10 +517,10 @@ if not os.path.isfile('./batch_run.txt'):  # We only visualize for runs of speci
     # ax.plot(time_simul_A, p_scaling, color='b')
 
     ax.scatter(time_simul_B, p_B, color='g')
-    p_scaling = []
-    for i in range(len(time_simul_A)):
-        p_scaling.append(0.2660 * time_simul_A[i] ** (-1 / 7))
-    ax.plot(time_simul_A, p_scaling, color='g')
+    # p_scaling = []
+    # for i in range(len(time_simul_A)):
+    #     p_scaling.append(0.2660 * time_simul_A[i] ** (-1 / 7))
+    # ax.plot(time_simul_A, p_scaling, color='g')
 
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
@@ -665,7 +630,7 @@ if not os.path.isfile('./batch_run.txt'):  # We only visualize for runs of speci
     my_X = 0.
     my_Y = 0.
     ext_pnts = np.empty((2, 2), dtype=np.float64)
-    fracture_list_slice_B = plot_fracture_list_slice(Fr_list_A,
+    fracture_list_slice_B = plot_fracture_list_slice(my_list,
                                                    variable='w',
                                                    projection='2D',
                                                    plot_cell_center=True,
@@ -679,7 +644,7 @@ if not os.path.isfile('./batch_run.txt'):  # We only visualize for runs of speci
     my_X = 0.
     my_Y = 0.
     ext_pnts = np.empty((2, 2), dtype=np.float64)
-    fracture_list_slice_B = plot_fracture_list_slice(Fr_list_A,
+    fracture_list_slice_B = plot_fracture_list_slice(my_list,
                                                    variable='w',
                                                    projection='2D',
                                                    plot_cell_center=True,

src/anisotropy.py
@@ -973,7 +973,7 @@ def get_toughness_from_zeroVertex(elts, mesh, mat_prop, alpha, l, zero_vrtx):
 #-----------------------------------------------------------------------------------------------------------------------
 
 
-def get_neighbors(NeiElements, elt):
+def get_8neighbors(NeiElements, elt):
     # neighbors
     #     6     5       4
     #     7    elt      3
@@ -993,14 +993,15 @@ def get_neighbors(NeiElements, elt):
     return neighbors_band
 #-----------------------------------------------------------------------------------------------------------------------
 import copy
-def process_closer_to_done(still_to_be_done, done, mesh, K1c, elts, frontLen, k):
+def process_closer_to_done(still_to_be_done, done, mesh, K1c, elts, k):
+    # this function computes the fracture toughness
     still_to_be_done_new = []
     done_new = copy.deepcopy(done)
     for i in still_to_be_done:
         i_indx = np.where(elts == i)[0]
 
-        # find the neighbors of the cell still to be done
-        nei = get_neighbors(mesh.NeiElements,i)
+        # find the 8 neighbors of the cell still to be done
+        nei = get_8neighbors(mesh.NeiElements,i)
 
         #if some of the neighbors have been done, find them
         nei_done = np.intersect1d(done, nei)
@@ -1008,7 +1009,6 @@ def process_closer_to_done(still_to_be_done, done, mesh, K1c, elts, frontLen, k)
             indexes = np.flatnonzero(np.in1d(elts, nei_done))
             if k < 1:
                 K1c[i_indx] = np.mean(K1c[indexes])
-                #K1c[i_indx] = np.dot(K1c[indexes],frontLen[indexes])/np.sum(frontLen[indexes])
             else:
                 K1c[i_indx] = np.mean(K1c[indexes])
 
@@ -1020,89 +1020,11 @@ def process_closer_to_done(still_to_be_done, done, mesh, K1c, elts, frontLen, k)
 #-----------------------------------------------------------------------------------------------------------------------
 
 
-def get_toughness_from_Front(Ffront, elts, fully_traversed, mesh, mat_prop, alpha):
+def get_toughness_from_Front(Ffront, tip_and_fully_trav, tip, fully_traversed, mesh, mat_prop, alpha, get_from_mid_front = False):
     """
     This function returns the toughness based on its values at the extremes of the segment defining the front (except for the fully traversed cells
     """
-
-    if mat_prop.K1cFunc is None:
-        return mat_prop.K1c[elts]
-
-    if mat_prop.anisotropic_K1c:
-        return mat_prop.K1cFunc(alpha)
-    else:
-        table = np.full(mesh.NumberOfElts, np.inf)
-        frontLen =  np.full(len(elts), np.inf)
-        #Ffront_centers = []
-        for i in range(len(Ffront)):
-            x = (Ffront[i, 0] + Ffront[i, 2]) / 2.
-            y = (Ffront[i, 1] + Ffront[i, 3]) / 2.
-            #Ffront_centers.append([x,y])
-            el_id = mesh.locate_element(x, y)[0]
-            table[el_id] = i
-            elts_id = np.where(elts == el_id)[0]
-            if len(elts_id) > 0:
-                frontLen[elts_id] = ((Ffront[i, 0] - Ffront[i, 2]) ** 2 + (Ffront[i, 1] - Ffront[i, 3]) ** 2) ** .5
-        # to close the fracture front
-        x = (Ffront[-1, 2] + Ffront[0, 0]) / 2.
-        y = (Ffront[-1, 3] + Ffront[0, 1]) / 2.
-        #Ffront_centers.append([x, y])
-        el_id = mesh.locate_element(x, y)[0]
-        table[el_id] = len(Ffront)
-        elts_id = np.where(elts==el_id)[0]
-        if len(elts_id) > 0:
-            frontLen[elts_id] = ((Ffront[-1, 2] - Ffront[0, 0]) ** 2 +(Ffront[-1, 3] - Ffront[0, 1]) ** 2) ** .5
-
-        # returning the Kprime according to the given function
-        K1c = np.empty((len(elts),), dtype=np.float64)
-        for i in range(0, len(elts)):
-            if elts[i] not in fully_traversed:
-                ind = int(table[elts[i]])
-                if ind == len(Ffront):
-                    x1 = Ffront[-1, 2] ; x2 = Ffront[0, 0]
-                    y1 = Ffront[-1, 3] ; y2 = Ffront[0, 1]
-                else:
-                    x1 = Ffront[ind, 2] ; x2 = Ffront[ind, 0]
-                    y1 = Ffront[ind, 3] ; y2 = Ffront[ind, 1]
-                #K1c[i] = (mat_prop.K1cFunc(x1, y1) + mat_prop.K1cFunc(x2, y2))/2.
-                K1c[i] = np.minimum(mat_prop.K1cFunc(x1, y1), mat_prop.K1cFunc(x2, y2))
-        # now defining K in the fully traversed
-        done = (np.setdiff1d(elts,fully_traversed)).tolist()
-        still_to_be_done = fully_traversed
-        end = False
-        k = 0
-        while not end:
-            if len(still_to_be_done) <1:
-                end = True
-            else:
-                still_to_be_done, done, K1c = process_closer_to_done(still_to_be_done, done, mesh, K1c, elts, frontLen, k)
-                k = k + 1
-
-
-        # from utility import plot_as_matrix
-        # K = np.zeros((mesh.NumberOfElts,), )
-        # K[elts] = K1c
-        # plot_as_matrix(K, mesh)
-
-        # from continuous_front_reconstruction import plot_xy_points
-        # background_Color = np.zeros(mesh.NumberOfElts)
-        # for ii in range(mesh.NumberOfElts):
-        #     background_Color[ii] = mat_prop.Kprime_func(mesh.CenterCoor[ii, 0], mesh.CenterCoor[ii, 1])
-        # plot_xy_points(np.arange(mesh.NumberOfElts), mesh, background_Color, elts, xstore, ystore, fig=None,
-        #                annotate_cellName=False, annotate_edgeName=False, annotatePoints=False, grid=True,
-        #                oldfront=Ffront, joinPoints=False, disregard_plus=True)
-        return K1c
-
-#-----------------------------------------------------------------------------------------------------------------------
-
-
-def get_toughness_from_midFront(Ffront, tip_and_fully_trav, tip, fully_traversed, mesh, mat_prop, alpha):
-    """
-    This function returns the toughness at the center of the segment defining the front (except for the fully traversed cells
-    tip_and_fully_trav --> contains fully_traversed and partially filled tips
-    tip --> contains partially filled tips
-    fully_traversed --> contains only fully_traversed
-    """
+    log = logging.getLogger('PyFrac.anisotropy.get_toughness_from_Front')
 
     if mat_prop.K1cFunc is None:
         return mat_prop.K1c[tip_and_fully_trav]
@@ -1110,37 +1032,32 @@ def get_toughness_from_midFront(Ffront, tip_and_fully_trav, tip, fully_traversed
     if mat_prop.anisotropic_K1c:
         return mat_prop.K1cFunc(alpha)
     else:
-        table = np.full(mesh.NumberOfElts, np.inf)
-        frontLen =  np.full(len(tip_and_fully_trav), np.inf)
-        Ffront_centers = []
-        for i in range(len(Ffront)):
-            x = (Ffront[i, 0] + Ffront[i, 2]) / 2.
-            y = (Ffront[i, 1] + Ffront[i, 3]) / 2.
-            Ffront_centers.append([x,y])
-            el_id = mesh.locate_element(x, y)[0]
-            table[el_id] = i
-            elts_id = np.where(tip == el_id)[0]
-            if len(elts_id) > 0:
-                frontLen[elts_id] = ((Ffront[i, 0] - Ffront[i, 2]) ** 2 + (Ffront[i, 1] - Ffront[i, 3]) ** 2) ** .5
-        # to close the fracture front
-        x = (Ffront[-1, 2] + Ffront[0, 0]) / 2.
-        y = (Ffront[-1, 3] + Ffront[0, 1]) / 2.
-        Ffront_centers.append([x, y])
-        el_id = mesh.locate_element(x, y)[0]
-        table[el_id] = len(Ffront)
-        elts_id = np.where(tip==el_id)[0]
-        if len(elts_id) > 0:
-            frontLen[elts_id] = ((Ffront[-1, 2] - Ffront[0, 0]) ** 2 +(Ffront[-1, 3] - Ffront[0, 1]) ** 2) ** .5
-
         # returning the Kprime according to the given function
         K1c = np.empty((len(tip_and_fully_trav),), dtype=np.float64)
         for i in range(0, len(tip_and_fully_trav)):
             if tip_and_fully_trav[i] not in fully_traversed:
-                ind = int(table[tip_and_fully_trav[i]])
-                [x,y] = Ffront_centers[ind]
-                K1c[i] = mat_prop.K1cFunc(x, y)
-        # now defining K in the fully traversed
-        done = tip.tolist()
+                ind = np.where(tip==tip_and_fully_trav[i])[0]
+                # assuming that Ffront is ordered as tip
+                # Ffront[-1] == tip[0]
+                # assuming that size Ffront == size tip
+                x1 = Ffront[ind-1, 2] ; x2 = Ffront[ind-1, 0]
+                y1 = Ffront[ind-1, 3] ; y2 = Ffront[ind-1, 1]
+                # checking the assumptions ----
+                xc=mesh.CenterCoor[tip[ind],0]; yc=mesh.CenterCoor[tip[ind],1]
+                xm = 0.5 * (x1 + x2) ; ym = 0.5 * (y1 + y2)
+
+                if not (xm <= xc+mesh.hx/2. and xm >= xc-mesh.hx/2. and ym <= yc + mesh.hy / 2. and ym >= yc - mesh.hy / 2.):
+                    log.warning("Violating the assumption that Ffront is ordered as tip list.")
+                    SystemExit("Violating the assumption that Ffront is ordered as tip list.")
+                # ----------------------------
+
+                #K1c[i] = (mat_prop.K1cFunc(x1, y1) + mat_prop.K1cFunc(x2, y2))/2.
+                if get_from_mid_front:
+                    K1c[i] = mat_prop.K1cFunc(xm, ym)
+                else:
+                    K1c[i] = np.minimum(mat_prop.K1cFunc(x1, y1), mat_prop.K1cFunc(x2, y2))
+        # now defining K in the fully traversed cells as the average of K in the cells where K has been computed
+        done = (copy.deepcopy(tip)).tolist()
         still_to_be_done = fully_traversed
         end = False
         k = 0
@@ -1148,7 +1065,7 @@ def get_toughness_from_midFront(Ffront, tip_and_fully_trav, tip, fully_traversed
             if len(still_to_be_done) <1:
                 end = True
             else:
-                still_to_be_done, done, K1c = process_closer_to_done(still_to_be_done, done, mesh, K1c, elts, frontLen, k)
+                still_to_be_done, done, K1c = process_closer_to_done(still_to_be_done, done, mesh, K1c, tip_and_fully_trav, k)
                 k = k + 1
 
 
@@ -1166,66 +1083,6 @@ def get_toughness_from_midFront(Ffront, tip_and_fully_trav, tip, fully_traversed
         #                oldfront=Ffront, joinPoints=False, disregard_plus=True)
         return K1c
 
-#-----------------------------------------------------------------------------------------------------------------------
-
-
-def get_toughness_from_midFront_zv(Ffront, elts, fully_traversed, mesh, mat_prop, alpha, l, zero_vrtx):
-    """
-    This function returns the toughness at the center of the segment defining the front (except for the fully traversed cells
-    """
-
-    if mat_prop.K1cFunc is None:
-        return mat_prop.K1c[elts]
-
-    if mat_prop.anisotropic_K1c:
-        return mat_prop.K1cFunc(alpha)
-    else:
-        table = np.full(mesh.NumberOfElts, np.inf)
-        for i in range(len(Ffront)):
-            x = (Ffront[i, 0] + Ffront[i, 2]) / 2.
-            y = (Ffront[i, 1] + Ffront[i, 3]) / 2.
-            table[mesh.locate_element(x, y)[0]] = i
-
-
-        # returning the Kprime according to the given function
-        K1c = np.empty((len(elts),), dtype=np.float64)
-        x = np.zeros((len(elts),), )
-        y = np.zeros((len(elts),), )
-
-        for i in range(0, len(elts)):
-            if elts[i] not in fully_traversed:
-                ind = int(table[elts[i]])
-                x[i] = (Ffront[ind,0] + Ffront[ind,2]) / 2.
-                y[i] = (Ffront[ind,1] + Ffront[ind,3]) / 2.
-                K1c[i] = mat_prop.K1cFunc(x[i], y[i])
-            else:
-                if zero_vrtx[i] == 0:
-                    x[i] = mesh.VertexCoor[mesh.Connectivity[elts[i], 0], 0] + l[i] * np.cos(alpha[i])
-                    y[i] = mesh.VertexCoor[mesh.Connectivity[elts[i], 0], 1] + l[i] * np.sin(alpha[i])
-                elif zero_vrtx[i] == 1:
-                    x[i] = mesh.VertexCoor[mesh.Connectivity[elts[i], 1], 0] - l[i] * np.cos(alpha[i])
-                    y[i] = mesh.VertexCoor[mesh.Connectivity[elts[i], 1], 1] + l[i] * np.sin(alpha[i])
-                elif zero_vrtx[i] == 2:
-                    x[i] = mesh.VertexCoor[mesh.Connectivity[elts[i], 2], 0] - l[i] * np.cos(alpha[i])
-                    y[i] = mesh.VertexCoor[mesh.Connectivity[elts[i], 2], 1] - l[i] * np.sin(alpha[i])
-                elif zero_vrtx[i] == 3:
-                    x[i] = mesh.VertexCoor[mesh.Connectivity[elts[i], 3], 0] + l[i] * np.cos(alpha[i])
-                    y[i] = mesh.VertexCoor[mesh.Connectivity[elts[i], 3], 1] - l[i] * np.sin(alpha[i])
-                K1c[i] = mat_prop.K1cFunc(x[i], y[i])
-
-        # from utility import plot_as_matrix
-        # K = np.zeros((mesh.NumberOfElts,), )
-        # K[elts] = K1c
-        # plot_as_matrix(K, mesh)
-
-        # from continuous_front_reconstruction import plot_xy_points
-        # background_Color = np.zeros(mesh.NumberOfElts)
-        # for ii in range(mesh.NumberOfElts):
-        #     background_Color[ii] = mat_prop.Kprime_func(mesh.CenterCoor[ii, 0], mesh.CenterCoor[ii, 1])
-        # plot_xy_points(np.arange(mesh.NumberOfElts), mesh, background_Color, elts, xstore, ystore, fig=None,
-        #                annotate_cellName=False, annotate_edgeName=False, annotatePoints=False, grid=True,
-        #                oldfront=Ffront, joinPoints=False, disregard_plus=True)
-        return K1c
 #-----------------------------------------------------------------------------------------------------------------------
 
 

src/continuous_front_reconstruction.py
@@ -705,11 +705,11 @@ def find_fictitius_cells(anularegion, NeiElements, sgndDist_k):
     """
     This function has vectorized operations.
     This function returns a list of "valid" "fictitius cells".
-    A fictitius cell is made of 4 cells of the mesh e.g. cells i,a,b,c in the mesh below.
-    A fictitius cell is represented by the name of the element in position i in the mesh below
-    A valid fictitius cell is a cell where at least one vertex has Level Set<0 and at least one has LS>0
-    A valid fictitius cell is important because we know the front is passing through it.
-    The front will always enter and exit the fictitius cell from two different edges.
+    A fictitious cell is made of 4 cells of the mesh e.g. cells i,a,b,c in the mesh below.
+    A fictitious cell is represented by the name of the element in position i in the mesh below
+    A valid fictitious cell is a cell where at least one vertex has Level Set<0 and at least one has LS>0
+    A valid fictitious cell is important because we know the front is passing through it.
+    The front will always enter and exit the fictitious cell from two different edges.
     The front can't exit the fictitious cell from a vertex because we set LS -(machine precision) where it was 0.
 
      _ _ _ _ _ _
@@ -2131,8 +2131,12 @@ def reconstruct_front_continuous(sgndDist_k, anularegion, Ribbon, eltsChannel, m
         """
         if np.any(sgndDist_k[mesh.Frontlist] < 0):
             log.warning('Some cells at the boundary of the mesh have negative level set')
-            negativefront = np.where(sgndDist_k < 0)[0]
-            intwithFrontlist = np.intersect1d(negativefront, np.asarray(mesh.Frontlist))
+            #cp: to be faster --
+            negLS_FrontlistIDs = np.where(sgndDist_k[mesh.Frontlist] < 0)[0]
+            intwithFrontlist = mesh.Frontlist[negLS_FrontlistIDs]
+            #negativefront = np.where(sgndDist_k < 0)[0]
+            #intwithFrontlist = np.intersect1d(negativefront, np.asarray(mesh.Frontlist))
+            #cp --
             correct_size_of_pstv_region = [False, True, False]
             return intwithFrontlist, None, None, None, None, None, None, None, correct_size_of_pstv_region, None, None, None, None
 
@@ -2148,7 +2152,7 @@ def reconstruct_front_continuous(sgndDist_k, anularegion, Ribbon, eltsChannel, m
 
         """
         1) - define a dictionary for the Ribbon cells
-           - find a list of valid fictitius cells (inamesOfFC) that can belongs to more than one fracture
+           - find a list of valid fictitius cells (inamesOfFC) that can belong to more than one fracture
            - compute the LS at the valid fictitius cells (LSofFC)
            - compute the "i names" of the FC of different types 
                Whe define the fictitius cell types: