Fixed bugs in level set calculation on a band. Worked on remeshing with factor != 2.

src/time_step/ts_explicit_front.py
@@ -93,6 +93,8 @@ def time_step_explicit_front(Fr_lstTmStp, C, Boundary, timeStep, Qin, mat_proper
     sgndDist_k = fmmStruct.LS
     sgndDist_k[toEval] = -sgndDist_k[toEval]
 
+    eval_region = np.where(sgndDist_k[front_region] >= -Fr_lstTmStp.mesh.cellDiag)[0]
+
     # Gets the new tip elements, along with the length and angle of the perpendiculars drawn on front (also containing
     # the elements which are fully filled after the front is moved outward)
     if sim_properties.projMethod == 'ILSA_orig':
@@ -120,7 +122,7 @@ def time_step_explicit_front(Fr_lstTmStp, C, Boundary, timeStep, Qin, mat_proper
             zrVertx_k_without_fully_traversed, \
             correct_size_of_pstv_region,\
             sgndDist_k_temp, Ffront,number_of_fronts, fronts_dictionary = reconstruct_front_continuous(sgndDist_k,
-                                                                          front_region,
+                                                                          front_region[eval_region],
                                                                           Fr_lstTmStp.EltRibbon,
                                                                           Fr_lstTmStp.EltChannel,
                                                                           Fr_lstTmStp.mesh,
@@ -176,6 +178,8 @@ def time_step_explicit_front(Fr_lstTmStp, C, Boundary, timeStep, Qin, mat_proper
                 # negative inside and positive outside.
                 sgndDist_k = fmmStruct.LS
                 sgndDist_k[toEval] = -sgndDist_k[toEval]
+
+                eval_region = np.where(sgndDist_k[front_region] >= -Fr_lstTmStp.mesh.cellDiag)[0]
 
         sgndDist_k = sgndDist_k_temp
         del correct_size_of_pstv_region