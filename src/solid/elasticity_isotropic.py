90346d18aa590d467ea76554a781462fad5ebb78
carlo peruzzo <carlo.peruzzo@epfl.ch>
2021-11-23 10:24:25 +0100

    Fixing the Isotropic toeplitz loading

--- src/solid/elasticity_isotropic.py
+++ src/solid/elasticity_isotropic.py
@@ -141,6 +141,9 @@ class load_isotropic_elasticity_matrix_toepliz(elasticity_matrix_toepliz):
 
     def __init__(self, Mesh, Ep, C_precision=np.float64, useHMATdot=False, nu=None):
 
+        self.nu = nu
+        self.Ep = Ep
+
         if useHMATdot:
             if nu is None: SystemExit("please, provide the Poisson's ratio to get the full blocks")
             self.HMATcreationTime = []
@@ -148,10 +151,7 @@ class load_isotropic_elasticity_matrix_toepliz(elasticity_matrix_toepliz):
         else:
             elas_prop_HMAT = []
 
-        self.nu = nu
-        self.Ep = Ep
         matprop = [Ep]
-
         super().__init__(Mesh, matprop, elas_prop_HMAT, C_precision, useHMATdot, kerneltype='Isotropic')
 
     def reload_toepliz_Coe(self, Lx, Ly, nx, ny, hx, hy, mat_prop):
