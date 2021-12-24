54a97675aa406c0b66e40855f0f56f2c0bb9f2f1
carlo peruzzo <carlo.peruzzo@epfl.ch>
2021-12-24 19:21:45 +0100

    Several optimizations of the elasticity dot product in the case of tip correction. Adding some examples to compare R0 and R4 kernels

--- 12_R0vsR4/rectangular_R0vsR4.py
+++ 12_R0vsR4/rectangular_R0vsR4.py
@@ -6,118 +6,40 @@ Created by Carlo Peruzzo on Tue Dec 15 10:18:56 2021.
 Copyright (c) "ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, Geo-Energy Laboratory", 2016-2022.
 All rights reserved. See the LICENSE.TXT file for more details.
 """
-
-# External imports
-import numpy as np
-from scipy.sparse.linalg import bicgstab
-import logging
+# external imports
 import time
-import matplotlib.pyplot as plt
 import json
 
 # local imports
-from utilities.postprocess_fracture import append_to_json_file
-from utilities.utility import plot_as_matrix
-from mesh_obj.mesh import CartesianMesh
-from solid.elasticity_isotropic import load_isotropic_elasticity_matrix_toepliz
-from utilities.utility import setup_logging_to_console
-from linear_solvers.preconditioners.prec_back_subst_EHL import EHL_iLU_Prec
-from linear_solvers.linear_iterative_solver import iteration_counter
+from common_rect_and_radial_tests import *
 from level_set.continuous_front_reconstruction import plot_two_fronts
-
-# setting up the verbosity level of the log at console
-setup_logging_to_console(verbosity_level='debug')
-log = logging.getLogger('PyFrac.solve_width_pressure')
-
-
-def plot_3d_scatter(zdata, xdata, ydata, zlabel = 'z', xlabel = 'x', ylabel = 'y'):
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-    ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='viridis')
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-    ax.set_zlabel(zlabel);
-    return fig
-
-# ----------------------------------------------
-def get_solution(C, p, EltCrack, x0=None):
-    """
-
-    :param C: elasticity matrix
-    :param p: pressure
-    :param EltCrack: IDs of the elements in the crack
-    :return: solution x of the system C.x = p
-    """
-
-    # prepare preconditioner
-    Aprec = EHL_iLU_Prec(C._get9stencilC(EltCrack))
-    counter = iteration_counter(log=log)  # to obtain the number of iteration and residual
-    C._set_domain_IDX(EltCrack)
-    C._set_codomain_IDX(EltCrack)
-
-    sol_ = bicgstab(C, p, x0=x0, M=Aprec, atol=10.e-14, tol=1.e-9, maxiter=1000, callback=counter)
-
-    if sol_[1] > 0:
-        print("     --> iterative solver did NOT converge after " + str(sol_[1]) + " iterations!")
-    elif sol_[1] == 0:
-        print("     --> iterative solver converged after " + str(counter.niter) + " iter. ")
-    return sol_[0]
+from utilities.postprocess_fracture import append_to_json_file
+from solid.elasticity_isotropic import load_isotropic_elasticity_matrix_toepliz
 
 
 # ----------------------------------------------
-def get_mesh(sim_info, refinement_ID):
-
-    ar = sim_info["aspect ratio"]
-    lx = sim_info["domain x"][1] - sim_info["domain x"][0]
-    ly = lx * ar
-    d_x = sim_info["domain x"]
-    d_y = [-ly / 2., ly / 2.]
-
-    # define nx
-    if refinement_ID < sim_info["n. of refinements x"]:
-        nx = refinement_ID * sim_info["nx min"]
-    else:
-        nx = sim_info["nx min"]
-
-    # define ny
-    if refinement_ID < sim_info["n. of refinements y"]:
-        ny = refinement_ID * sim_info["nx min"] * ar
-    else:
-        ny = sim_info["nx min"] * ar
-
-    # define ny should be an odd number
-    if ny % 2 == 0 : ny = ny + 1
-    if nx % 2 == 0 : nx = nx + 1
-
-    return CartesianMesh(d_x, d_y, nx, ny)
-
-
-# ----------------------------------------------
-run = False
+run = True
 file_name = "results_rectangular.json"
 
 if run:
-    sim_info = {}
-
-    # deciding the aspect ratio
+    # deciding the aspect ratio (if as > 1 -> Ly > Lx)
     # - it is Ly/Lx
-    sim_info["aspect ratio"] = 2
-
     # number of mesh refinements
     #   - along x and y
-    sim_info["n. of refinements x"] = 10
-    sim_info["n. of refinements y"] = 10
+
+    sim_info = {"aspect ratio": 20, "n. of refinements x": 5, "n. of refinements y": 5}
+
     sim_info["max. n. of refinements"] = np.maximum(sim_info["n. of refinements x"], sim_info["n. of refinements y"])
 
     # the coarsest mesh (the y direction will be a function of x)
     sim_info["nx min"] = 7
 
     # set the domain size (the y direction will be a function of x)
-    sim_info["domain x"] = [-2,2]
+    sim_info["domain x"] = [-100, 100]
 
     # solid properties
     sim_info["nu"] = 0.4                                                    # Poisson's ratio
-    sim_info["youngs mod"] = 3.3e10                                         # Young's modulus
+    sim_info["youngs mod"] = 3.3e4                                          # Young's modulus
     sim_info["Eprime"] = sim_info["youngs mod"] / (1 - sim_info["nu"]**2)   # plain strain modulus
 
     # uniform load
@@ -133,7 +55,12 @@ if run:
                "max w R4" : [],
                "frac volume R0": [],
                "frac volume R4": [],
-               "n. of Elts" : []}
+               "n. of Elts" : [],
+               "nu": sim_info["nu"],  # Poisson's ratio
+               "youngs mod": sim_info["youngs mod"],
+               "H" : sim_info["domain x"][1] - sim_info["domain x"][0],
+               "p": sim_info["p"],
+               }
 
     for refinement_ID in np.arange(1, maxref, 1):
 
@@ -144,7 +71,10 @@ if run:
         # creating mesh & plotting
         Mesh = get_mesh(sim_info, refinement_ID)
         EltCrack = np.arange(Mesh.NumberOfElts)
-        #plot_two_fronts(Mesh, newfront=None, oldfront=None , fig=None, grid=True, cells = EltCrack, my_marker = " ")
+
+        if refinement_ID == 1 or  refinement_ID == maxref -1:
+           plot_two_fronts(Mesh, newfront=None, oldfront=None , fig=None, grid=True, cells = EltCrack, my_marker = " ")
+
         results["n. of Elts"].append(int(len(EltCrack)))
         results["nx"].append(int(Mesh.nx))
         results["ny"].append(int(Mesh.ny))
@@ -179,14 +109,14 @@ if run:
         st = st + 1
         print(f" {st}) solving R_0 matrix")
         dummy = - time.time()
-        sol_R0 = get_solution(C_R0, p, EltCrack, x0=sol_R4)
+        sol_R0 = get_solution(C_R0, p, EltCrack)
         dummy = dummy + time.time()
         print(f"     --> done in {dummy}")
 
         # some plots
-        #rel_err_num = 100 * np.abs(sol_R0 - sol_R4) / sol_R4
-        #plot_as_matrix(rel_err_num, mesh=Mesh) # 2D plot
-        #plot_3d_scatter(sol_R4, Mesh.CenterCoor[:, 0], Mesh.CenterCoor[:, 1]) # 3D plot
+        # rel_err_num = 100 * np.abs(sol_R0 - sol_R4) / sol_R4
+        # plot_as_matrix(rel_err_num, mesh=Mesh) # 2D plot
+        # plot_3d_scatter(sol_R4, Mesh.CenterCoor[:, 0], Mesh.CenterCoor[:, 1]) # 3D plot
 
         st = st + 1
         print(f" {st}) saving stats.")
@@ -199,7 +129,7 @@ if run:
     print("Saving to file")
     content = results
     action = 'dump_this_dictionary'
-    append_to_json_file(file_name, content, action, delete_existing_filename=True)
+    append_to_json_file(file_name, [content], action, delete_existing_filename=True)
 
 if not run:
     with open(file_name, "r+") as json_file:
@@ -209,30 +139,49 @@ print("Plotting results")
 
 # w max
 fig1 = plt.figure()
-plt.suptitle('w_max VS DOF')
+plt.suptitle('Rectangular crack test')
 plt.plot(results["n. of Elts"], results["max w R0"], c='r', marker="+")
 plt.plot(results["n. of Elts"], results["max w R4"], c='b', marker="+")
-plt.xlabel('# of DOF')
+
+y_ana = wmax_plane_strain_solution(results["youngs mod"], results["nu"],results["p"], results["H"])
+y_ana = np.full(2, y_ana)
+plt.plot([results["n. of Elts"][0],results["n. of Elts"][-1]], y_ana, c='black', marker=" ")
+plt.xlabel('# of DOF in the crack')
 plt.ylabel('w max')
-plt.legend(('R0', 'R4'),loc='upper center', shadow=True)
+plt.legend(('R0 - tip corr not needed', 'R4 - tip corr not needed'),loc='lower left', shadow=True)
 plt.xscale('log')
 plt.yscale('log')
 
 # volume
 fig1 = plt.figure()
-plt.suptitle('fractue volume VS DOF')
+plt.suptitle('Rectangular crack test')
 plt.plot(results["n. of Elts"], results["frac volume R0"], c='r', marker="+")
 plt.plot(results["n. of Elts"], results["frac volume R4"], c='b', marker="+")
-plt.xlabel('# of DOF')
+plt.xlabel('# of DOF in the crack')
 plt.ylabel('frac volume')
-plt.legend(('R0', 'R4'),loc='upper center', shadow=True)
+plt.legend(('R0 - tip corr not needed', 'R4 - tip corr not needed'), loc='lower left', shadow=True)
+plt.xscale('log')
+plt.yscale('log')
+
+# rel err w max
+fig1 = plt.figure()
+plt.suptitle('Rectangular crack test')
+
+y_ana = wmax_plane_strain_solution(results["youngs mod"], results["nu"],results["p"], results["H"])
+# plt.plot(results["n. of Elts"], 100 * np.abs(np.asarray(results["max w R0"]) - y_ana)/y_ana, c='r', marker="+")
+# plt.plot(results["n. of Elts"], 100 * np.abs(np.asarray(results["max w R4"]) - y_ana)/y_ana, c='b', marker="+")
+plt.plot(results["nx"], 100 * np.abs(np.asarray(results["max w R0"]) - y_ana)/y_ana, c='r', marker="+")
+plt.plot(results["nx"], 100 * np.abs(np.asarray(results["max w R4"]) - y_ana)/y_ana, c='b', marker="+")
+plt.tick_params(labeltop=True, labelright=True)
+plt.grid(True, which="both", ls="-")
+
+plt.xlabel('# of DOF in the crack')
+plt.ylabel('rel. err. w max [%]')
+plt.legend(('R0 - tip corr not needed', 'R4 - tip corr not needed'), loc='lower left', shadow=True)
 plt.xscale('log')
 plt.yscale('log')
 
 print(" <<<< DONE >>>>")
-
-
-
 
 #
 # rel_err_R4_R0 = []
---
+++ src/solid/elasticity_tip_correction.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+This file is part of PyFrac.
+
+Created by Carlo Peruzzo on Tue Dec 15 10:18:56 2021.
+Copyright (c) "ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, Geo-Energy Laboratory", 2016-2022.
+All rights reserved. See the LICENSE.TXT file for more details.
+"""
+
+# external imports
+import numpy as np
+from numba import prange, njit
+
+@njit(parallel=True, fastmath=True, nogil=True, cache=True)
+def tip_correction_factors(FillFrac):
+    """
+    This function is used to apply the <<filling fraction tip correction>> according to:
+    Rider & Napier, 1985, Error Analysis and Design of Large-Scale Tabular Mining Stress Analyser.
+
+    :param FillFrac: vector containing the filling fraction
+    :return: the diagonal factors such that
+
+             with NO tip correction:
+                C * v = res
+
+
+             WITH tip correction (two steps procedure):
+                indxs = EltTip (EltTip=list of tip elements IDs)
+                C * v = res
+                res[indxs] = res[indxs] + C[0,0] * diag_corr * v[indxs]
+
+             Note:
+                - C[0,0] is the self effect in the elasticity matrix. Remember that it is constant.
+                - the above procedure is equivalent to:
+                    for e in EltTip:
+                        C[EltTip[e], EltTip[e]] *= (1. + diag_corr[e])
+                    C * v = res
+                - this function is applied to provide a correction to all the elements at the front:
+                   diag_corr[e] == 0. if FillFrac[e] == 1.
+                               != 0. if FillFrac[e] < 1.
+
+
+    """
+
+    coeff = np.pi / 4.
+
+    n_EltTip = len(FillFrac)
+    diag_corr = np.zeros(n_EltTip)
+
+    r = FillFrac - 0.25
+
+    for e in prange(n_EltTip):
+        r_e = r[e]
+        if r_e < 0.1:
+            r_e = 0.1
+        ac = (1. - r_e) / r_e
+        diag_corr[e] = ac * coeff
+
+    return diag_corr
