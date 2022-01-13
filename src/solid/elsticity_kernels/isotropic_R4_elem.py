Fixing the computation of the tractions for the R4 elem

src/solid/elsticity_kernels/isotropic_R4_elem.py
@@ -172,55 +172,13 @@ def get_toeplitzCoe_isotropic_R4(nx, ny, hx, hy, matprop, C_precision):
     a = hx / 2.
     b = hy / 2.
 
-    ## THIS IS THE NAIVE WAY:
-    # for i in prange(ny):
-    #     dyk1 = (i - 2) * hy
-    #     dyk2 = i * hy
-    #     dyk3 = (i + 2) * hy
-    #     for j in prange(nx):
-    #         dx1k = (j - 2) * hx
-    #         dx2k = j * hx
-    #         dx3k = (j + 2) * hx
-    #                                                        # dx    dy                         dx    dy                         dx    dy
-    #         C_toeplitz_coe[i * nx + j] = sig_zz_Dz_11(a, b, dx1k, dyk1) + sig_zz_Dz_12(a, b, dx1k, dyk2) + sig_zz_Dz_13(a, b, dx1k, dyk3) + \
-    #                                      sig_zz_Dz_21(a, b, dx2k, dyk1) + sig_zz_Dz_22(a, b, dx2k, dyk2) + sig_zz_Dz_23(a, b, dx2k, dyk3) + \
-    #                                      sig_zz_Dz_31(a, b, dx3k, dyk1) + sig_zz_Dz_32(a, b, dx3k, dyk2) + sig_zz_Dz_33(a, b, dx3k, dyk3)
-
     for ind in prange(ny*nx):
         # ind = i * nx + j
         # we get back i and j
         i = ind // nx
         j = ind - i * nx
-
-        # dyk1 = (i - 2) * hy
-        # dyk2 = i * hy
-        # dyk3 = (i + 2) * hy
-        #
-        # dx1k = (j - 2) * hx
-        # dx2k = j * hx
-        # dx3k = (j + 2) * hx
-        #                                                # dx    dy                         dx    dy                         dx    dy
-        # C_toeplitz_coe[ind] = sig_zz_Dz_11(a, b, dx1k, dyk1) + sig_zz_Dz_12(a, b, dx1k, dyk2) + sig_zz_Dz_13(a, b, dx1k, dyk3) + \
-        #                       sig_zz_Dz_21(a, b, dx2k, dyk1) + sig_zz_Dz_22(a, b, dx2k, dyk2) + sig_zz_Dz_23(a, b, dx2k, dyk3) + \
-        #                       sig_zz_Dz_31(a, b, dx3k, dyk1) + sig_zz_Dz_32(a, b, dx3k, dyk2) + sig_zz_Dz_33(a, b, dx3k, dyk3)
-
-        # dx1 = (j + 1) * hx
-        # dx2 = j * hx
-        # dx3 = (j - 1) * hx
-        #
-        # dy1 = (i + 1) * hy
-        # dy2 = i * hy
-        # dy3 = (i - 1) * hy
-        # C_toeplitz_coe[ind] = sig_zz_Dz_11(a, b, dx1, dy1) + sig_zz_Dz_12(a, b, dx1, dy2) + sig_zz_Dz_13(a, b, dx1, dy3) + \
-        #                       sig_zz_Dz_21(a, b, dx2, dy1) + sig_zz_Dz_22(a, b, dx2, dy2) + sig_zz_Dz_23(a, b, dx2, dy3) + \
-        #                       sig_zz_Dz_31(a, b, dx3, dy1) + sig_zz_Dz_32(a, b, dx3, dy2) + sig_zz_Dz_33(a, b, dx3, dy3)
-        # C_toeplitz_coe[ind] = sig_zz_Dz_11(a, b, dx1, dy1) + sig_zz_Dz_21(a, b, dx1, dy2) + sig_zz_Dz_31(a, b, dx1, dy3) + \
-        #                       sig_zz_Dz_12(a, b, dx2, dy1) + sig_zz_Dz_22(a, b, dx2, dy2) + sig_zz_Dz_32(a, b, dx2, dy3) + \
-        #                       sig_zz_Dz_13(a, b, dx3, dy1) + sig_zz_Dz_23(a, b, dx3, dy2) + sig_zz_Dz_33(a, b, dx3, dy3)
-
         dx = j * hx
         dy = i * hy
-
         C_toeplitz_coe[0, ind] = isotropic_R4_kernel(dx, dy, hx, hy, a, b, const)
         C_toeplitz_coe[1, ind] = isotropic_R4_kernel(-dx, dy, hx, hy, a, b, const)
     #return const * C_toeplitz_coe
@@ -251,21 +209,12 @@ def get_R4_normal_traction_at(xy_obs, xy_crack, w_crack, Ep, hx, hy):
         ind_crack = global_ind - ind_obs * n_xy_crack
         xy_obs_i = xy_obs[ind_obs,:]
         xy_crack_i = xy_crack[ind_crack,:]
+
         dx_i, dy_i = get_distance_components(xy_obs_i[0], xy_obs_i[1], xy_crack_i[0], xy_crack_i[1])
 
-        dyk1 = dy_i - 2. * hy
-        dyk2 = dy_i
-        dyk3 = dy_i + 2. * hy
+        normal_trac[ind_obs, ind_crack] = w_crack[ind_crack] * isotropic_R4_kernel(dx_i, dy_i, hx, hy, a, b, const)
 
-        dx1k = dx_i - 2. * hx
-        dx2k = dx_i
-        dx3k = dx_i + 2. * hx
-                                                       # dx    dy                         dx    dy                         dx    dy
-        normal_trac[ind_obs,ind_crack] = w_crack[ind_crack] * (sig_zz_Dz_11(a, b, dx1k, dyk1) + sig_zz_Dz_12(a, b, dx1k, dyk2) + sig_zz_Dz_13(a, b, dx1k, dyk3) +
-                              sig_zz_Dz_21(a, b, dx2k, dyk1) + sig_zz_Dz_22(a, b, dx2k, dyk2) + sig_zz_Dz_23(a, b, dx2k, dyk3) +
-                              sig_zz_Dz_31(a, b, dx3k, dyk1) + sig_zz_Dz_32(a, b, dx3k, dyk2) + sig_zz_Dz_33(a, b, dx3k, dyk3))
-
-    return const * np.sum(normal_trac, axis=1)
+    return np.sum(normal_trac, axis=1)
 
 
 