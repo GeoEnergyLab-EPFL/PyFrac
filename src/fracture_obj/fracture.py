Fixed the efficiency calculation for width constrained cells.

src/fracture_obj/fracture.py
@@ -942,7 +942,10 @@ class Fracture:
             Fr_coarse.LkOff = LkOff
             Fr_coarse.LkOffTotal = self.LkOffTotal
             Fr_coarse.injectedVol = self.injectedVol
-            Fr_coarse.efficiency = (Fr_coarse.injectedVol - Fr_coarse.LkOffTotal) / Fr_coarse.injectedVol
+            Fr_coarse.efficiency = Fr_coarse.mesh.EltArea * (np.sum(Fr_coarse.w[Fr_coarse.EltTip] * Fr_coarse.FillF) +
+                                                             np.sum(Fr_coarse.w[Fr_coarse.EltChannel])) \
+                                   / Fr_coarse.injectedVol
+            # Fr_coarse.efficiency = (Fr_coarse.injectedVol - Fr_coarse.LkOffTotal) / Fr_coarse.injectedVol
             Fr_coarse.time = self.time
             Fr_coarse.closed = np.asarray([])
             Fr_coarse.wHist = wHist_coarse

src/time_step/ts_explicit_front.py
@@ -678,7 +678,9 @@ def time_step_explicit_front(Fr_lstTmStp, C, Boundary, timeStep, Qin, mat_proper
     Fr_kplus1.LkOff = LkOff
     Fr_kplus1.LkOffTotal += np.sum(LkOff)
     Fr_kplus1.injectedVol += sum(Qin) * timeStep
-    Fr_kplus1.efficiency = (Fr_kplus1.injectedVol - Fr_kplus1.LkOffTotal) / Fr_kplus1.injectedVol
+    Fr_kplus1.efficiency = Fr_kplus1.mesh.EltArea * (np.sum(Fr_kplus1.w[Fr_kplus1.EltTip] * Fr_kplus1.FillF) +
+                                                     np.sum(Fr_kplus1.w[Fr_kplus1.EltChannel])) / Fr_kplus1.injectedVol
+    # Fr_kplus1.efficiency = (Fr_kplus1.injectedVol - Fr_kplus1.LkOffTotal) / Fr_kplus1.injectedVol
     Fr_kplus1.source = np.where(Qin != 0)[0]
 
     if sim_properties.saveRegime: