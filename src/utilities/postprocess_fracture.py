Fixed some issues with the calculation of the power dissipations.

src/utilities/postprocess_fracture.py
@@ -1848,10 +1848,13 @@ def get_energy_split(Solid, Fluid, SimProp, Fr_list): #AM 2022, based on CP rout
     Viscous_EN = init_list_of_objects(len(Fr_list)-1)
     Fracture_EN = init_list_of_objects(len(Fr_list)-1)
     Elastic_EN = init_list_of_objects(len(Fr_list)-1)
+    Elastic_Stress_EN = init_list_of_objects(len(Fr_list)-1)
     External_EN = init_list_of_objects(len(Fr_list)-1)
     Internal_EN = init_list_of_objects(len(Fr_list)-1)
     LeakOff_EN = init_list_of_objects(len(Fr_list)-1)
     energy_time = init_list_of_objects(len(Fr_list)-1)
+
+    External_EN_inj = init_list_of_objects(len(Fr_list) - 1)
 
     # 4) loop on the fractures and get the different time steps
     NoT = len(Fr_list)
@@ -1888,26 +1891,23 @@ def get_energy_split(Solid, Fluid, SimProp, Fr_list): #AM 2022, based on CP rout
             l, x_m, y_m, x, y = get_l_Ffront_ordered_as_v(fr_i.Ffront, fr_i.EltTip, fr_i_mesh)
 
             Viscous_EN[iter] = get_Viscous_EN(fr_i, Fluid, Solid, SimProp, fr_i_mesh)
-            Fracture_EN[iter] = get_Fracture_EN(fr_i, Solid, fr_i_mesh, l, x_m, y_m)
-            Elastic_EN[iter] = get_Elastic_EN(fr_im1, fr_i, fr_i_mesh)
+            Fracture_EN[iter] = get_Fracture_EN(fr_i, Solid, l, x_m, y_m)
+            Elastic_EN[iter] = get_Elastic_EN(fr_im1, fr_i, fr_i_mesh, fr_i.pNet, fr_im1.pNet)
+            Elastic_Stress_EN[iter] = 2 * get_Elastic_EN(fr_im1, fr_i, fr_i_mesh, fr_i.pFluid-fr_i.pNet,
+                                                  fr_im1.pFluid-fr_im1.pNet)
             External_EN[iter] = get_External_EN(fr_im1, fr_i, Fluid, Solid, SimProp, fr_i_mesh, x, y)
-            Internal_EN[iter] = Viscous_EN[iter]+Fracture_EN[iter]+Elastic_EN[iter]
+            Internal_EN[iter] = Viscous_EN[iter]+Fracture_EN[iter]+Elastic_EN[iter]+Elastic_Stress_EN[iter]
+
+            External_EN_inj[iter] = get_External_gravity(fr_i, Fluid, Solid, SimProp, fr_i_mesh, x, y)
             # Not yet implemented!
             # if sum(fr_i.leakOff) != 0:
             #     LeakOff_EN[iter] = get_leakOff_EN(fr_im1, fr_i)
             energy_time[iter] = fr_i.time
-            if fr_i.time > .85e-5 and fr_i.time < 1.25e-5:
-                print("hey")
-            if fr_i.time > .85e-2 and fr_i.time < 1.25e-2:
-                print("hey")
-            if fr_i.time > 45 and fr_i.time < 60:
-                print("hey")
-            if fr_i.time > 0.9e6 and fr_i.time < 1.1e6:
-                print("hey")
             iter = iter + 1
     end_energy = iter
     return External_EN[:end_energy], Internal_EN[:end_energy], Viscous_EN[:end_energy], Fracture_EN[:end_energy],\
-           Elastic_EN[:end_energy], LeakOff_EN[:end_energy], energy_time[:end_energy]
+           Elastic_EN[:end_energy], LeakOff_EN[:end_energy], Elastic_Stress_EN[:end_energy], energy_time[:end_energy], \
+           External_EN_inj[:end_energy]
 
 #-----------------------------------------------------------------------------------------------------------------------
 
@@ -2037,7 +2037,7 @@ def get_l_Ffront_ordered_as_v(Ffront, EltTip, mesh):
 
 #-----------------------------------------------------------------------------------------------------------------------
 
-def get_Fracture_EN(fr_i, Solid,fr_i_mesh, l, x_m, y_m):
+def get_Fracture_EN(fr_i, Solid, l, x_m, y_m):
     """This function calculates the energy spent to create new fractures for a current time-step.
 
     :param fr_i: fracture object of the current time step - see related documentation
@@ -2082,7 +2082,7 @@ def get_Fracture_EN(fr_i, Solid,fr_i_mesh, l, x_m, y_m):
 
 #-----------------------------------------------------------------------------------------------------------------------
 
-def get_Elastic_EN(fr_im1, fr_i, fr_i_mesh):
+def get_Elastic_EN(fr_im1, fr_i, fr_i_mesh, traction_i, traction_im1):
     """This function calculates the Elastic power stored in the system.
 
     :param fr_im1: fracture object of the previous time step - see related documentation
@@ -2095,18 +2095,16 @@ def get_Elastic_EN(fr_im1, fr_i, fr_i_mesh):
     dt = np.abs(fr_i.time - fr_im1.time)
     cell_area = fr_i_mesh.hx * fr_i_mesh.hy
     w = fr_i.w[fr_i.EltCrack]
-    pf = fr_i.pFluid[fr_i.EltCrack]
     nEltCrack=fr_i.EltCrack.size
     Elastic_EN_vec = np.zeros(nEltCrack)
     for i in range(nEltCrack):
         ID=fr_i.EltCrack[i]
         w_old = fr_im1.w[ID]
-        pf_old = fr_im1.pFluid[ID]
         if not ID in fr_i.EltTip:
-            Elastic_EN_vec[i] = 0.5 * (w[i]*pf[i]-pf_old*w_old)/dt
+            Elastic_EN_vec[i] = 0.5 * (w[i]*traction_i[ID]-traction_im1[ID]*w_old)/dt
         else:
             tip_ind = np.where(fr_i.EltTip==ID)[0]
-            Elastic_EN_vec[i] = fr_i.FillF[tip_ind] * 0.5 * (w[i]*pf[i]-pf_old*w_old)/dt
+            Elastic_EN_vec[i] = fr_i.FillF[tip_ind] * 0.5 * (w[i]*traction_i[ID]-traction_im1[ID]*w_old)/dt
     Elastic_EN = cell_area * np.sum(Elastic_EN_vec)
 
     return Elastic_EN
@@ -2123,7 +2121,7 @@ def get_External_EN(fr_im1, fr_i, Fluid, Solid, SimProp, fr_i_mesh, x, y):
     # Note: 1) this is coded up for only one injection point
     if SimProp.gravity:
         return get_External_injection(fr_im1, fr_i) + \
-               get_External_gravity(fr_im1, fr_i, Fluid, Solid, SimProp, fr_i_mesh, x, y)
+               get_External_gravity(fr_i, Fluid, Solid, SimProp, fr_i_mesh, x, y)
     else:
         return get_External_injection(fr_im1, fr_i)
 
@@ -2145,7 +2143,7 @@ def get_External_injection(fr_im1, fr_i):
 
 #-----------------------------------------------------------------------------------------------------------------------
 
-def get_External_gravity(fr_im1, fr_i, Fluid, Solid, SimProp, fr_i_mesh, x, y):
+def get_External_gravity(fr_i, Fluid, Solid, SimProp, fr_i_mesh, x, y):
     """This function calculates the external power added to the system.
 
     :param fr_im1: fracture object of the previous time step - see related documentation
@@ -2171,18 +2169,20 @@ def get_External_gravity(fr_im1, fr_i, Fluid, Solid, SimProp, fr_i_mesh, x, y):
     for i in range(nEltCrack):
         ID = fr_i.EltCrack[i]
         if not ID in fr_i.EltTip:
-            Vy[i] = (fluid_vel[1, i] + fluid_vel[3, i] + fluid_vel[5, i] + fluid_vel[7, i])/4.
-            External_EN_gravity_vec[i] = Vy[i] * w[i] * rho_f * gravity
+            # Vy[i] = (fluid_vel[1, i] + fluid_vel[3, i] + fluid_vel[5, i] + fluid_vel[7, i])/4.
+            Vy[i] = (fluid_vel[5, i] + fluid_vel[7, i]) / 2.
+            External_EN_gravity_vec[i] = Vy[i] * w[i] * rho_f * (-gravity)
         else:
             tip_ind = np.where(fr_i.EltTip==ID)[0][0]
-            coord_zero_vertex = fr_i_mesh.CenterCoor[fr_i.ZeroVertex[tip_ind]]
+            coord_zero_vertex = fr_i_mesh.VertexCoor[fr_i_mesh.Connectivity[ID][fr_i.ZeroVertex[tip_ind]]]
             [alpha, xint, yint] = findangle(x[tip_ind][0], y[tip_ind][0], x[tip_ind][1], y[tip_ind][1],
                                             coord_zero_vertex[0], coord_zero_vertex[1],
                                             100*np.sqrt(np.finfo(float).eps))
             normal_x = xint - coord_zero_vertex[0]
             normal_y = yint - coord_zero_vertex[1]
             normal_y = normal_y/np.sqrt(normal_x ** 2 + normal_y ** 2)
-            External_EN_gravity_vec[i] = fr_i.FillF[tip_ind] * w[i] * rho_f * fr_i.v[tip_ind] * normal_y * (-gravity)
+            External_EN_gravity_vec[i] = fr_i.FillF[tip_ind] * w[i] * rho_f * np.abs(fr_i.v[tip_ind]) * normal_y *\
+                                         (-gravity)
 
 
     # the viscous energy is the cell area times the parallel plate viscosity (12 \mu) * the square of the flow velocity.