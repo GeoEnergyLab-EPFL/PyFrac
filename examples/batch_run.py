450e64b1635dc434e68df15fc481ac9b86913b8c
Pedro Lima <pedrolima.ec@outlook.com>
2023-11-21 11:18:11 +0100

    Adds a summary of working examples to batch_run.py

--- examples/batch_run.py
+++ examples/batch_run.py
@@ -17,21 +17,31 @@ import socket
 import subprocess
 
 # Create (and closes) a dummy file to know it is a batch_run
-with open('batch_run.txt', 'w') as fp:
+with open("batch_run.txt", "w") as fp:
     pass
 fp.close()
 
 # generate the list with all examples
-example_list = glob.glob('*.py')
-example_list.remove('batch_run.py')
+example_list = glob.glob("*.py")
+example_list.remove("batch_run.py")
 
 # get the name of the file
-filename = 'timing__' + socket.gethostname() + '__' + datetime.datetime.now().strftime("%Y-%m-%d__%H_%M_%S")
-filename = filename.replace('.', '_')
+filename = (
+    "Data/timing__"
+    + socket.gethostname()
+    + "__"
+    + datetime.datetime.now().strftime("%Y-%m-%d__%H_%M_%S")
+)
+filename = filename.replace(".", "_")
+
+summaryFileName = (
+    "Data/allExamplesSummary__"
+    + datetime.datetime.now().strftime("%Y-%m-%d__%H_%M_%S")
+    + ".csv"
+)
 
 # Open the timing file. The name gives the Machine and the starting time
-with open(filename, 'w') \
-        as output:
+with open(filename, "w") as output:
     output.write("++++++++++++++ Batch run ++++++++++++++\n")
 
     # set the time when the batch_run started
@@ -39,31 +49,37 @@ with open(filename, 'w') \
 
     # Loop over the examples
     for example in example_list:
-        output.write('\n')
-        output.write('+++++++++++++++++++++++++++++++++++++++\n')
-        output.write('Example ' + example + '\n')
+        output.write("\n")
+        output.write("+++++++++++++++++++++++++++++++++++++++\n")
+        output.write("Example " + example + "\n")
 
         # Start time of the example
         example_start = datetime.datetime.now()
 
         # Running the example
-        subprocess.call(["python3", example])
-
+        returnValue = subprocess.call(["python3", example])
+        successfullQ = returnValue == 0
+        # Summarize working examples to csv
+        with open(
+            summaryFileName,
+            "a",
+        ) as summaryFile:
+            summaryFile.write(example + ", " + str(successfullQ) + "\n")
         # write the runtime and the time since the start of the example to the log file
         example_runtime = datetime.datetime.now() - example_start
-        output.write('Time for example: ' + str(example_runtime) + '\n')
+        output.write("Time for example: " + str(example_runtime) + "\n")
         time_since_start = datetime.datetime.now() - batch_start
-        output.write('Time since batch start: ' + str(time_since_start) + '\n')
-        output.write('+++++++++++++++++++++++++++++++++++++++\n')
+        output.write("Time since batch start: " + str(time_since_start) + "\n")
+        output.write("+++++++++++++++++++++++++++++++++++++++\n")
 
         # closes figures if still open
         if len(plt.get_fignums()) > 0:
-            plt.close('all')
+            plt.close("all")
 
     # close the log file
     output.close()
 
 # remove the dummy file (Very important! for normal runs we check if it exists or not!)
-os.remove('batch_run.txt')
+os.remove("batch_run.txt")
 
 sys.exit(0)
